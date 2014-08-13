eb352a0b2818f98596e17dec1fc89e820f334f9c
Eric Kersten <ekersten@gmail.com>
2014-08-12 23:34:12 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>DAV UTN</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+        <link rel="stylesheet" href="css/dav.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <!--[if lt IE 9]><script src="js/vendor/rem.min.js"></script><![endif]-->
+    </head>
+    <body>
+        <header>
+            <div class="row">
+                <div class="column medium-4">
+                    <img src="img/logo_dav.png" alt="Departamento de Aprendizaje Visual" width="230">
+                </div>
+                <div class="column medium-8">
+                    <nav>
+                        <ul>
+                            <li><a href="#">Quienes<br/>Somos</a></li>
+                            <li><a href="#">Programas para<br/>Escuelas</a></li>
+                            <li><a href="#">Programa de Entes de<br/>Gobierno y R.S.E.</a></li>
+                            <li><a href="#">Prensa</a></li>
+                            <li><a href="#">Contacto</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <section class="home">
+            
+        </section>
+        <footer>
+            
+        </footer>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <!-- <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script> -->
+    </body>
+</html>
