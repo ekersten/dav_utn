2e54b20e030890c0ac61f4111f55da2407ac2a94
Eric Kersten <ekersten@gmail.com>
2014-08-13 23:51:19 -0300

    avances home

--- index.php
+++ index.php
@@ -11,13 +11,26 @@
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
         <link rel="stylesheet" href="css/dav.css">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <script src="js/vendor/modernizr.js"></script>
         <!--[if lt IE 9]><script src="js/vendor/rem.min.js"></script><![endif]-->
     </head>
     <body>
         <?php include("includes/header.php"); ?>
         <section class="home">
-            
+            <div class="hero">
+                <div class="row">
+                    <div class="column medium-4 medium-push-8">
+                        <div class="contenido">
+                            <div class="interno">
+                                <h1>Digital Junior</h1>
+                                <p>Proyecto de Certificación de Conocimientos Informaticos<br/>"Mucho más que rendir un examen"</p>
+                                <a href="#" class="boton">Conocer más</a>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
         </section>
         <?php include("includes/footer.php"); ?>
         <!-- local scripts -->
