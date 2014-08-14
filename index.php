a5afa04c2ab0feca3db166a408f2afef7a328559
Eric Kersten <ekersten@gmail.com>
2014-08-13 23:09:54 -0300

    header y footer

--- index.php
+++ index.php
@@ -24,8 +24,26 @@
                     <nav>
                         <ul>
                             <li><a href="#">Quienes<br/>Somos</a></li>
-                            <li><a href="#">Programas para<br/>Escuelas</a></li>
-                            <li><a href="#">Programa de Entes de<br/>Gobierno y R.S.E.</a></li>
+                            <li>
+                                <a href="#">Programas para<br/>Escuelas</a>
+                                <ul>
+                                    <li><a href="#">Item 1</a></li>
+                                    <li><a href="#">Item 2</a></li>
+                                    <li><a href="#">Item 3</a></li>
+                                    <li><a href="#">Item 4</a></li>
+                                    <li><a href="#">Item 5</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">Programa de Entes de<br/>Gobierno y R.S.E.</a>
+                                <ul>
+                                    <li><a href="#">Item 1</a></li>
+                                    <li><a href="#">Item 2</a></li>
+                                    <li><a href="#">Item 3</a></li>
+                                    <li><a href="#">Item 4</a></li>
+                                    <li><a href="#">Item 5</a></li>
+                                </ul>
+                            </li>
                             <li><a href="#">Prensa</a></li>
                             <li><a href="#">Contacto</a></li>
                         </ul>
@@ -37,7 +55,17 @@
             
         </section>
         <footer>
-            
+            <div class="row">
+                <div class="column small-2">
+                    <img src="img/logo_utn.png" width="80" alt="Universidad Tecnológica Nacional"/>
+                </div>
+                <div class="column small-10">
+                    <div class="row">
+                        <div class="column medium-6"><p>UTN.BA Departamento de Aprendizaje Visual<br/>Facultad Regional de Buenos Aires<br/>Secretaría de Cultura y Extensión Universitaria</p></div>
+                        <div class="column medium-6"><p>Av. Medrano 951 2&ordm; Piso<br/>Horario de Atención: Lunes a Viernes de 9 a 15 Hs.<br/>Teléfonos: (011) 4867-7565 / 7545</p></div>
+                    </div>
+                </div>
+            </div>
         </footer>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
