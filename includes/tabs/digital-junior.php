65c50d1d6c14e28f8e1aa61749d58d592f4ffb91
Eric Kersten <ekersten@gmail.com>
2014-08-18 10:32:48 -0300

    adaptacion tabs

---
+++ includes/tabs/digital-junior.php
@@ -0,0 +1,58 @@
+<ul class="tabs">
+	<li class="tab">
+		<div class="holder"></div>
+		<div class="gradient"></div>
+		<div class="content">
+			<table>
+				<tr>
+					<td>
+						<span class="icon icon-docs"></span>
+						<span class="text">Informacion General</span>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</li>
+	<li class="tab">
+		<div class="holder"></div>
+		<div class="gradient"></div>
+		<div class="content">
+			<table>
+				<tr>
+					<td>
+						<span class="icon">C</span>
+						<span class="text">Contacto</span>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</li>
+	<li class="tab">
+		<div class="holder"></div>
+		<div class="gradient"></div>
+		<div class="content">
+			<table>
+				<tr>
+					<td>
+						<span class="icon">E</span>
+						<span class="text">Empresa</span>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</li>
+	<li class="tab">
+		<div class="holder"></div>
+		<div class="gradient"></div>
+		<div class="content">
+			<table>
+				<tr>
+					<td>
+						<span class="icon">N</span>
+						<span class="text">Nosotros</span>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</li>
+</ul>
