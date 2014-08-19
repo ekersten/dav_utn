35ae39c6467b8da9712bf57c8517f68a1de772cf
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-18 22:27:43 -0300

    kimtech

---
+++ includes/tabs/kimtech.php
@@ -0,0 +1,50 @@
+<ul class="tabs">
+	<li class="tab">
+		<div class="holder"></div>
+		<div class="gradient"></div>
+		<div class="content">
+			<table>
+				<tr>
+					<td>
+						<a href="digital-junior-informacion.php">
+							<span class="icon icon-docs"></span>
+							<span class="text">Informacion General</span>
+						</a>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</li>
+	<li class="tab">
+		<div class="holder"></div>
+		<div class="gradient"></div>
+		<div class="content">
+			<table>
+				<tr>
+					<td>
+						<a href="digital-junior-contenidos.php">
+							<span class="icon icon-bubbles"></span>
+							<span class="text">Opinion alumnos</span>
+						</a>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</li>
+	<li class="tab">
+		<div class="holder"></div>
+		<div class="gradient"></div>
+		<div class="content">
+			<table>
+				<tr>
+					<td>
+						<a href="digital-junior-distancia.php">
+							<span class="icon icon-checklist"></span>
+							<span class="text">Proyectos</span>
+						</a>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</li>
+</ul>
