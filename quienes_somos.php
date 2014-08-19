51b072022406763edd874799d2fbbae782a81d37
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-18 22:27:30 -0300

    quienes somos

---
+++ quienes_somos.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Digital Junior - DAV UTN</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+		<link rel="stylesheet" href="css/dav.css">
+		<script src="js/vendor/modernizr.js"></script>
+		<!--[if lt IE 9]><script src="js/vendor/rem.min.js"></script><![endif]-->
+	</head>
+	<body>
+		<?php include("includes/header.php"); ?>
+		<section class="tpl-2-col digital-junior">
+			<div class="header-image"></div>
+			<!-- <div class="subnav" data-activa="1">
+				<?php include("includes/tabs/digital-junior.php"); ?>
+			</div> -->
+			<div class="row contenedor">
+				<div class="column medium-9 pricipal">
+					<h1>Quienes Somos</h1>
+					<!-- <ul class="breadcrumbs">
+						<li><a href="#">Programa para escuelas</a></li>
+						<li><a href="#">Digital Junior</a></li>
+						<li>Información General</li>
+					</ul> -->
+					<!-- <blockquote>Proyecto de Certificación de Conocimientos Informáticos<br/>&ldquo;Mucho más que rendir un examen&rdquo;</blockquote> -->
+					<p>En el contexto de la realidad actual se hace necesario reflexionar sobre la escuela media, no sólo por las disfunciones que varios estudios han señalado, sino también en el marco de los procesos de modernización y desarrollo. </p>
+					<p>Se acusa a esta etapa de preparar mal a los estudiantes tanto para los estudios superiores como para la entrada al mercado laboral.</p>
+					<p>Una educación media pensada en el marco señalado hace necesario el compromiso de los directivos y docentes de asumir nuevos roles para que los centros educativos se transformen en verdaderos espacios de formación y crecimiento.</p>
+					<p>Es por ello que creemos que ha llegado el momento de generar puentes entre la escuela media y las Universidades con el objetivo de contribuir a colocar a la educación a la altura del actual desarrollo Científico-Tecnológico, privilegiando la aplicación de estrategias no tradicionales. </p>
+					<p>DAV – Departamento de Aprendizaje Visual, a través de su proyecto HOMOVIDENS, pretende ser el punto de unión entre los recursos de la Facultad Regional Buenos Aires de la Universidad Tecnológica Nacional y las necesidades de soluciones en Nuevas Tecnologías Aplicadas a la Educación de toda la Comunidad Educativa.</p>
+					<h2>¿Cómo nace HOMOVIDENS?</h2>
+					<p>HOMOVIDENS es un proyecto desarrollado por el Departamento de Aprendizaje Visual (DAV) de la UTN.BA que nace con el objetivo de contribuir en mejorar los niveles de conocimiento en ciencias básicas incorporando nuevas formas de enseñanza y de aprendizaje.</p>
+					<p>El proyecto surge en el año 2002, a partir de la comprensión de los cambios que las TIC estaban generando a nivel mundial, y viendo la realidad educativa nacional, en especial la problemática en los niveles de conocimiento en ciencias básicas manifestados en los ingresos a la universidad. </p>
+					<p>En una primera etapa, se trabajó en la investigación y el diagnóstico de la realidad educativa actual, de los niveles de conocimiento, del nivel de uso de las TIC en las prácticas educativas y de las metodologías de enseñanza aplicadas a las propuestas educativas. A partir de aquí, comprendimos que estamos formando a ciudadanos del siglo XXI, con metodologías y programas del siglo XIX y pretendiendo incorporar las tecnologías más avanzadas.</p>
+					<p>Esta incompatibilidad, sumada a la gran cantidad de ofertas de capacitación con el sólo objetivo de incorporar la tecnología en las aulas como un fin en sí mismo, o hacer uso de programas informáticos en forma no productiva,  nos movilizó a la generación de un proyecto diferente.</p>
+					<h2>¿Qué es HOMOVIDENS hoy?</h2>
+					<p>HOMOVIDENS se convirtió en un proyecto integral que está formado por varios programas que fueron pensados para distintos miembros de la comunidad educativa</p>
+				</div>
+				<div class="column medium-3 secundario">
+					<div class="video">
+						<img src="img/video_dj.jpg" alt=""> <!-- reemplazar por video real -->
+					</div>
+					<p>El Programa <em>Digital Junior</em> le brinda los siguientes <strong>BENEFICIOS</strong></p>
+					<ul>
+						<li>Contenidos Actualizados</li>
+						<li>Capacitación docente</li>
+						<li>Materiales de estudio</li>
+						<li>Certificación de conocimientos</li>
+						<li>Asesoramiento</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+		<?php include("includes/footer.php"); ?>
+		<!-- local scripts -->
+	</body>
+</html>
