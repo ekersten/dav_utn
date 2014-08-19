700ab658da913cb0924fecb641529638ec7c1315
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-19 15:57:07 -0300

    escuelas

---
+++ escuela-informacion.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Escuela 2.0 - DAV UTN</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+		<link rel="stylesheet" href="css/dav.css">
+		<script src="js/vendor/modernizr.js"></script>
+		<!--[if lt IE 9]><script src="js/vendor/rem.min.js"></script><![endif]-->
+	</head>
+	<body>
+		<?php include("includes/header.php"); ?>
+		<section class="tpl-2-col escuela">
+			<div class="header-image"></div>
+			<div class="subnav" data-activa="1">
+				<?php include("includes/tabs/escuela.php"); ?>
+			</div>
+			<div class="row contenedor">
+				<div class="column medium-9 pricipal">
+					<h1>Información General</h1>
+					<ul class="breadcrumbs">
+						<li><a href="#">Programa para escuelas</a></li>
+						<li><a href="#">Escuela 2.0</a></li>
+						<li>Información General</li>
+					</ul>
+					<p>El desafío de incluir la tecnología en la educación está más vigente que nunca, sin embargo seguimos enseñando de la misma forma. Ante esta realidad cabe preguntarnos: ¿seguimos invirtiendo en equipamiento tecnológico y contenidos digitales, sosteniendo las rutinas tradicionales del docente? ¿O comenzamos a transformar la Escuela?</p>
+					<p>ESCUELA 2.0 es un programa desarrollado por la UTN.BA, pensado para transformar la Escuela  valiéndose del potencial de las nuevas tecnologías, para brindar un servicio educativo que trascienda las paredes de sus aulas. Una educación para el siglo XXI.</p>
+					<p>Este programa está pensado con el objetivo de colaborar con el Colegio para lograr un cambio efectivo, contemplando su contexto educativo, los recursos técnicos y humanos. Acompañándolos desde la concepción del nuevo proyecto en la capacitación docente y en el monitoreo de su implementación, logrando instaurar un modelo que transforme a corto plazo las posibilidades educativas de la institución integrando nuevas tecnologías Poniendo el acento en diseñar nuevas estrategias pedagógicas.</p>
+					<p>Un eje de trabajo del programa es la construcción del Campus Virtual del colegio. El mismo contempla una capacitación virtual, que permite a los docentes adquirir herramientas para desarrollar proyectos de aplicación áulica propios a través de una plataforma Moodle, aprendiendo a su vez  a diseñar, administrar y tutorear aulas virtuales.</p>
+					<p>Un segundo eje del programa propone el diseño de un proyecto que permite incorporar innovación pedagógica en diversas áreas de la currícula escolar, teniendo en cuenta los recursos humanos y el contexto que posee la institución. A modo de citar algunos ejemplos:
+						<ul>
+							<li>Simuladores Digitales: posibilitan el armado de laboratorios virtuales para la enseñanza de asignaturas exactas.</li>
+							<li>Realidad Aumentada: materiales digitales con 3D, proyectos de geolocalización, involucrando a las áreas sociales.</li>
+							<li>Análisis de Videos: facilitan ver el mundo real a través de la matemática y la física. La aplicación en los deportes genera un valor agregado en esta metodología.</li>
+							<li>Herramientas Web 2.0: permiten el desarrollo de trabajos colaborativos.</li>
+						</ul>
+					</p>
+					<p>Todos los ejes del programa tienen como objetivo que el docente experimente y adquiera nuevas capacidades posibles de transferir a los alumnos. En el hacer, el docente aprende, obtiene confianza y autonomía, logrando crear infinitas y originales propuestas de trabajo dentro y fuera del aula.</p>
+				</div>
+				<div class="column medium-3 secundario">
+					<div class="video">
+						<img src="img/video_escuela.jpg" alt=""> <!-- reemplazar por video real -->
+					</div>
+					<p>El Programa <em>Digital Junior</em> le brinda los siguientes <strong>BENEFICIOS</strong></p>
+					<ul>
+						<li>Contenidos Actualizados</li>
+						<li>Capacitación docente</li>
+						<li>Materiales de estudio</li>
+						<li>Certificación de conocimientos</li>
+						<li>Asesoramiento</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+		<?php include("includes/footer.php"); ?>
+		<!-- local scripts -->
+		<script src="js/tabs.js"></script>
+	</body>
+</html>
---
+++ escuela-proyectos.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Escuela 2.0 - DAV UTN</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+		<link rel="stylesheet" href="css/dav.css">
+		<script src="js/vendor/modernizr.js"></script>
+		<!--[if lt IE 9]><script src="js/vendor/rem.min.js"></script><![endif]-->
+	</head>
+	<body>
+		<?php include("includes/header.php"); ?>
+		<section class="tpl-2-col escuela">
+			<div class="header-image"></div>
+			<div class="subnav" data-activa="3">
+				<?php include("includes/tabs/escuela.php"); ?>
+			</div>
+			<div class="row contenedor">
+				<div class="column medium-9 pricipal">
+					<h1>Proyectos</h1>
+					<ul class="breadcrumbs">
+						<li><a href="#">Programa para escuelas</a></li>
+						<li><a href="#">Escuela 2.0</a></li>
+						<li>Proyectos</li>
+					</ul>
+					<h2>PROXIMAMENTE</h2>
+				</div>
+				<div class="column medium-3 secundario">
+					<div class="video">
+						<img src="img/video_escuela.jpg" alt=""> <!-- reemplazar por video real -->
+					</div>
+					<p>El Programa <em>Digital Junior</em> le brinda los siguientes <strong>BENEFICIOS</strong></p>
+					<ul>
+						<li>Contenidos Actualizados</li>
+						<li>Capacitación docente</li>
+						<li>Materiales de estudio</li>
+						<li>Certificación de conocimientos</li>
+						<li>Asesoramiento</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+		<?php include("includes/footer.php"); ?>
+		<!-- local scripts -->
+		<script src="js/tabs.js"></script>
+	</body>
+</html>
