eb85b85cd54976f2b4964165d6f50c0b38a92e64
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-18 22:28:07 -0300

    contacto

---
+++ contacto.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Digital Junior - DAV UTN</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+		<link rel="stylesheet" href="css/dav.css">
+		<script src="js/vendor/modernizr.js"></script>
+		<!--[if lt IE 9]><script src="js/vendor/rem.min.js"></script><![endif]-->
+	</head>
+	<body>
+		<?php include("includes/header.php"); ?>
+		<section class="tpl-2-col contacto">
+			<div class="header-image"></div>
+			<div class="row contenedor">
+				<h1>Contacto</h1>
+				<div class="column medium-9 pricipal">
+					<div class="row">
+						<div class="column medium-6">
+							<p>
+								<label>Nombre</label>
+								<input type="text" name="nombre" value="">
+							</p>
+							<p>
+								<label>Mail</label>
+								<input type="mail" name="mail" value="">
+							</p>
+							<p>
+								<label>Teléfono</label>
+								<input type="phone" name="tel" value="">
+							</p>
+							<p>
+								<label>Colegio</label>
+								<input type="text" name="colegio" value="">
+							</p>
+						</div>
+						<div class="column medium-6">
+							<p>
+								<label>Ciudad</label>
+								<input type="text" name="ciudad" value="">
+							</p>
+							<p>
+								<label>Consulta</label>
+								<textarea name="consulta" rows="4"></textarea>
+							</p>
+						</div>
+						<div class="row">
+							<input type="submit" name="" value="Enviar">
+							<input type="submit" name="" value="Cancelar">
+								
+							</div>	
+					</div>
+					<!-- <ul class="breadcrumbs">
+						<li><a href="#">Programa para escuelas</a></li>
+						<li><a href="#">Digital Junior</a></li>
+						<li>Información General</li>
+					</ul> -->
+					<!-- <blockquote>Proyecto de Certificación de Conocimientos Informáticos<br/>&ldquo;Mucho más que rendir un examen&rdquo;</blockquote> -->
+					
+				</div>
+				<div class="column medium-3 secundario">
+					<h2>Datos Adicionales</h2>
+					<p>Ing. Jorge Alfredo Almiña </p>
+					<p>Director a cargo</p>
+					<p>Ing. Soledad Grisel Corrales</p>
+					<p>Coordinadora General</p>
+					<p>El Programa <em>Digital Junior</em> le brinda los siguientes <strong>BENEFICIOS</strong></p>
+					<ul>
+						<li>Contenidos Actualizados</li>
+						<li>Capacitación docente</li>
+						<li>Materiales de estudio</li>
+						<li>Certificación de conocimientos</li>
+						<li>Asesoramiento</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+		<?php include("includes/footer.php"); ?>
+		<!-- local scripts -->
+		
+	</body>
+</html>
