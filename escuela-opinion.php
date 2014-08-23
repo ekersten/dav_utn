793ef33d7d76492ac0eb360a7e19596472699dda
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-22 22:30:19 -0300

    escuela 2.0

--- escuela-opinion.php
+++ escuela-opinion.php
@@ -33,7 +33,7 @@
 				</div>
 				<div class="column medium-3 secundario">
 					<div class="video">
-                        <a href="https://www.youtube.com/watch?v=sK2yxQDI0D0" class="fancybox-media"><img src="img/video_dj.jpg" alt=""> </a>
+                        <a href="https://www.youtube.com/watch?v=sK2yxQDI0D0" class="fancybox-media"><img src="img/video_escuela.jpg" alt=""> </a>
                     </div>
 					<p>El Programa <em>Digital Junior</em> le brinda los siguientes <strong>BENEFICIOS</strong></p>
 					<ul>
