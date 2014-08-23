9248f29881353bcb09d318c4bcca2766882b4ced
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-22 22:29:22 -0300

    quienes somos

--- quienes_somos.php
+++ quienes_somos.php
@@ -16,11 +16,8 @@
 	</head>
 	<body>
 		<?php include("includes/header.php"); ?>
-		<section class="tpl-2-col digital-junior">
+		<section class="tpl-2-col quienes">
 			<div class="header-image"></div>
-			<!-- <div class="subnav" data-activa="1">
-				<?php include("includes/tabs/digital-junior.php"); ?>
-			</div> -->
 			<div class="row contenedor">
 				<div class="column medium-9 pricipal">
 					<h1>Quienes Somos</h1>
@@ -29,23 +26,13 @@
 						<li><a href="#">Digital Junior</a></li>
 						<li>Información General</li>
 					</ul> -->
-					<!-- <blockquote>Proyecto de Certificación de Conocimientos Informáticos<br/>&ldquo;Mucho más que rendir un examen&rdquo;</blockquote> -->
-					<p>En el contexto de la realidad actual se hace necesario reflexionar sobre la escuela media, no sólo por las disfunciones que varios estudios han señalado, sino también en el marco de los procesos de modernización y desarrollo. </p>
-					<p>Se acusa a esta etapa de preparar mal a los estudiantes tanto para los estudios superiores como para la entrada al mercado laboral.</p>
-					<p>Una educación media pensada en el marco señalado hace necesario el compromiso de los directivos y docentes de asumir nuevos roles para que los centros educativos se transformen en verdaderos espacios de formación y crecimiento.</p>
-					<p>Es por ello que creemos que ha llegado el momento de generar puentes entre la escuela media y las Universidades con el objetivo de contribuir a colocar a la educación a la altura del actual desarrollo Científico-Tecnológico, privilegiando la aplicación de estrategias no tradicionales. </p>
-					<p>DAV – Departamento de Aprendizaje Visual, a través de su proyecto HOMOVIDENS, pretende ser el punto de unión entre los recursos de la Facultad Regional Buenos Aires de la Universidad Tecnológica Nacional y las necesidades de soluciones en Nuevas Tecnologías Aplicadas a la Educación de toda la Comunidad Educativa.</p>
-					<h2>¿Cómo nace HOMOVIDENS?</h2>
-					<p>HOMOVIDENS es un proyecto desarrollado por el Departamento de Aprendizaje Visual (DAV) de la UTN.BA que nace con el objetivo de contribuir en mejorar los niveles de conocimiento en ciencias básicas incorporando nuevas formas de enseñanza y de aprendizaje.</p>
-					<p>El proyecto surge en el año 2002, a partir de la comprensión de los cambios que las TIC estaban generando a nivel mundial, y viendo la realidad educativa nacional, en especial la problemática en los niveles de conocimiento en ciencias básicas manifestados en los ingresos a la universidad. </p>
-					<p>En una primera etapa, se trabajó en la investigación y el diagnóstico de la realidad educativa actual, de los niveles de conocimiento, del nivel de uso de las TIC en las prácticas educativas y de las metodologías de enseñanza aplicadas a las propuestas educativas. A partir de aquí, comprendimos que estamos formando a ciudadanos del siglo XXI, con metodologías y programas del siglo XIX y pretendiendo incorporar las tecnologías más avanzadas.</p>
-					<p>Esta incompatibilidad, sumada a la gran cantidad de ofertas de capacitación con el sólo objetivo de incorporar la tecnología en las aulas como un fin en sí mismo, o hacer uso de programas informáticos en forma no productiva,  nos movilizó a la generación de un proyecto diferente.</p>
-					<h2>¿Qué es HOMOVIDENS hoy?</h2>
-					<p>HOMOVIDENS se convirtió en un proyecto integral que está formado por varios programas que fueron pensados para distintos miembros de la comunidad educativa</p>
+					<blockquote>&ldquo;Un espacio especialmente creado para la articulación entre la Universidad y las Escuelas.&rdquo;</blockquote>
+					<p>DAV – Departamento de Aprendizaje Visual, a través de su proyecto HOMOVIDENS, se concibió en el año 2002 como punto de unión entre los recursos de la Facultad Regional Buenos Aires de la Universidad Tecnológica Nacional y las necesidades de soluciones en Nuevas Tecnologías Aplicadas a la Educación de toda la Comunidad Educativa. Generando puentes entre la escuela media y la Universidad con el objetivo de contribuir a colocar a la educación a la altura del actual desarrollo Científico-Tecnológico, privilegiando la aplicación de estrategias no tradicionales integrando TIC en las propuestas educativas acordes a los contextos de aplicación.</p>
+					<p>En la actualidad, y luego de haber recorrido 12 años de trabajo, el Departamento ofrece un proyecto integral formado por varios programas pensados para distintos miembros de la comunidad educativa, en los cuales nos acompañan más de 300 escuelas.</p>
 				</div>
-				<div class="column medium-3 secundario">
+		<!-- 		<div class="column medium-3 secundario">
 					<div class="video">
-						<img src="img/video_dj.jpg" alt=""> <!-- reemplazar por video real -->
+						<img src="img/video_dj.jpg" alt=""> 
 					</div>
 					<p>El Programa <em>Digital Junior</em> le brinda los siguientes <strong>BENEFICIOS</strong></p>
 					<ul>
@@ -55,7 +42,7 @@
 						<li>Certificación de conocimientos</li>
 						<li>Asesoramiento</li>
 					</ul>
-				</div>
+				</div> -->
 			</div>
 		</section>
 		<?php include("includes/footer.php"); ?>
