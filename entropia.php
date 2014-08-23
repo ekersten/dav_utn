92def7d80adc9b78db1b71fa6a27330c9281cb1d
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-22 22:30:32 -0300

    entropia

---
+++ entropia.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Entropia - DAV UTN</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+		<link rel="stylesheet" href="css/dav.css">
+		<script src="js/vendor/modernizr.js"></script>
+		<!--[if lt IE 9]><script src="js/vendor/rem.min.js"></script><![endif]-->
+	</head>
+	<body>
+		<?php include("includes/header.php"); ?>
+		<section class="tpl-2-col entropia">
+			<div class="header-image"></div>
+			<!-- <div class="subnav" data-activa="1">
+				<?php include("includes/tabs/digital-junior.php"); ?>
+			</div> -->
+			<div class="row contenedor">
+				<div class="column medium-9 pricipal">
+					<h1>ENTROPIA</h1>
+					<ul class="breadcrumbs">
+						<li><a href="#">Programa de Entes de Gobierno y R.S.E</a></li>
+						<li>Entropia</li>
+						<!-- <li>Información General</li> -->
+					</ul>
+					<p>Entropía es un programa de capacitación y nivelación, orientado a estudiantes que se encuentran cursando el último año del nivel secundario en escuelas públicas, que propone una transformación y evolución de los procesos tradicionales de enseñanza y aprendizaje en Ciencias Básicas. Este programa es desarrollado por el DAV, en conjunto con  el  Área de Desarrollo Social de la Secretaría de Cultura y Extensión Universitaria.</p>
+					<p>El impulso de esta iniciativa es lograr que un mayor porcentaje de alumnos de sectores de bajos recursos puedan acceder a carreras de ingeniería,  pretendiendo no solo contribuir a satisfacer una demanda del mercado profesional, sino también colaborar con la ruptura de los círculos de pobreza. El objetivo último perseguido por Entropía es igualar las posibilidades de muchos estudiantes, que por razones socioeconómicas y culturales, ven a la Facultad de Ingeniería como algo de difícil alcance.</p>
+					<p>La forma de trabajo planteada por este programa propone una preparación previa al examen de ingreso a la Universidad. La misma contempla  la participación en clases presenciales, sumadas al uso de un campus virtual, que permite a los alumnos trabajar en cualquier momento y lugar, fuera de la institución escolar. Durante este trayecto, los alumnos podrán trabajar con diversos sistemas y contenidos digitales, más motivadores. Este nuevo escenario de trabajo ha logrado mejores niveles de abstracción con materiales tradicionales, desarrollados didáctica y pedagógicamente por docentes de la UTN.BA en colaboración con Samsung Electronics Argentina. Por otro lado, la empresa fue la encargada de equipar tecnológicamente   el aula que permite el desarrollo de las clases a través del Campus Virtual.</p>
+					<p>A su vez, durante este trayecto, los alumnos son acompañados por estudiantes avanzados de la UTN.BA, quienes participan del programa cumpliendo la función de tutores y realizando apoyo y seguimiento a los alumnos, y que a su vez son supervisados por docentes graduados de la Universidad, con el objetivo de mejorar su rendimiento académico y asegurar la continuidad en todas las instancias del programa. </p>
+					<p>La primera experiencia de Entropía se está llevando adelante en la sede Lugano de la FRBA, con la participación de 35 alumnos de escuelas técnicas y comerciales de  la Ciudad Autónoma de Buenos Aires, y con la participación de cuatro estudiantes de las carreras de Ingeniería Industrial y Eléctrica oficiando como tutores.</p>
+				</div>
+			<!-- 	<div class="column medium-3 secundario">
+					<div class="video">
+						<a href="https://www.youtube.com/watch?v=-dxWBb0NVbo" class="fancybox-media"><img src="img/video_dj.jpg" alt=""> </a>
+					</div>
+					<p>El Programa <em>Digital Junior</em> le brinda los siguientes <strong>BENEFICIOS</strong></p>
+					<ul>
+						<li>Contenidos Actualizados</li>
+						<li>Capacitación docente</li>
+						<li>Materiales de estudio</li>
+						<li>Certificación de conocimientos</li>
+						<li>Asesoramiento</li>
+					</ul>
+				</div> -->
+			</div>
+		</section>
+		<?php include("includes/footer.php"); ?>
+		<!-- local scripts -->
+		<script src="js/tabs.js"></script>
+	</body>
+</html>
