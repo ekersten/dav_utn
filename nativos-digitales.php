0134fac2a706c15056a8dd0e653cc58a352a5e57
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-22 22:29:39 -0300

    NATIVOS

---
+++ nativos-digitales.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Nativos Digitales - DAV UTN</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+		<link rel="stylesheet" href="css/dav.css">
+		<script src="js/vendor/modernizr.js"></script>
+		<!--[if lt IE 9]><script src="js/vendor/rem.min.js"></script><![endif]-->
+	</head>
+	<body>
+		<?php include("includes/header.php"); ?>
+		<section class="tpl-2-col nativos">
+			<div class="header-image"></div>
+			<!-- <div class="subnav" data-activa="1">
+				<?php include("includes/tabs/digital-junior.php"); ?>
+			</div> -->
+			<div class="row contenedor">
+				<div class="column medium-9 pricipal">
+					<h1>Nativos Digitales</h1>
+					<ul class="breadcrumbs">
+						<li><a href="#">Programa de Entes de Gobierno y R.S.E</a></li>
+						<li>Nativos Digitales</li>
+						<!-- <li>Información General</li> -->
+					</ul>
+					<p>Este programa está basado en DOS principios:</p>
+					<h4>1 •	La necesidad del aprendizaje no solo de los lenguajes informáticos, sino de la lógica de los mismos.</h4>
+					<li>Los medios de comunicación y las tecnologías poseen una estructura fundamentalmente lingüística, en la actualidad los alumnos y jóvenes no poseen un dominio de lo lingüístico, sino de lo visual. Este programa se basa en la creencia de que los alumnos deben aprender a manejar las formas básicas de los lenguajes informáticos y de las lógicas en la que se apoyan, ya que contribuyen a que cualquier alumno o joven, tenga la posibilidad de adquirir ciertas formas de pensamiento útil para manejarse en las nuevas sociedades del conocimiento, o sea que les permite manejarse y no ser manejados por la tecnología. Es por ello que consideramos que los jóvenes deben aprender a comprender y eventualmente controlar mínimamente el funcionamiento informático. Expertos franceses sostienen “puesto que el nivel de abstracción es menor que el de las matemáticas modernas” y útil didácticamente, ya que la informática proporciona ejemplos concretos de las nociones matemáticas, “tanto en el sistema de funcionamiento como en la formulación de los algoritmos. Útil además para las profesiones que se ejercerán más tarde (...) Útil en resumen, y más allá del futuro empleo de la informática, como ‘manera de pensar’ (Marenco/Urvoy 1975:102-103)</p>
+					<h4>2 •La realidad es compleja, es por ello que los alumnos deben resolver situaciones que les permitan enfrentar la complejidad e integrar la información.</h4>
+					<p>La realidad es compleja, no está dividida en disciplinas y especialidades. Cuando un científico se enfrenta con una problemática o situación a resolver, debe integrar saberes disciplinarios porque la realidad está formada por la suma de ellos. Los alumnos buscan el conocimiento y la aplicación del pensamiento complejo a través de la resolución de problemas complejos. 
+Partiendo de la necesidad de programar una situación problema, integrar algunos de los bloques disociados en la enseñanza.</p>
+				<h5>Aquí es que comprendimos la existencia de dos necesidades a partir de las cuales surgió el proyecto:</h5>
+				<p>Una de estas necesidades es la de tener en cuenta, cómo expresa el Dr. Diego Levis, la dimensión lingüística de la informática en tanto técnica intelectual, que apunta a una alfabetización Integral. </p>
+				<p>Apoyando esta postura, sostenemos la necesidad de los alumnos de aprender las formas básicas de los lenguajes informáticos y de las lógicas en la que se apoyan, ya que contribuyen a que cualquier alumno o joven.</p>
+				<p>Otras de las necesidades detectadas que dan origen a ND es que desde la Universidad, y desde determinadas Empresas, percibimos que en los alumnos existe una falta de ciertas competencias básicas de informática y lógica, requeridas en el mercado laboral actual y para ingresar a las Universidades. </p>
+				<p>La demanda técnica del mercado requiere perfiles de formación que la escuela no está brindando, este programa pretende acercar a los jóvenes a esta demanda a través de un programa que les permita descubrir conceptos de programación aplicados a las ciencias básicas.</p>
+				</div>
+			<!-- 	<div class="column medium-3 secundario">
+					<div class="video">
+						<a href="https://www.youtube.com/watch?v=-dxWBb0NVbo" class="fancybox-media"><img src="img/video_dj.jpg" alt=""> </a>
+					</div>
+					<p>El Programa <em>Digital Junior</em> le brinda los siguientes <strong>BENEFICIOS</strong></p>
+					<ul>
+						<li>Contenidos Actualizados</li>
+						<li>Capacitación docente</li>
+						<li>Materiales de estudio</li>
+						<li>Certificación de conocimientos</li>
+						<li>Asesoramiento</li>
+					</ul>
+				</div> -->
+			</div>
+		</section>
+		<?php include("includes/footer.php"); ?>
+		<!-- local scripts -->
+		<script src="js/tabs.js"></script>
+	</body>
+</html>
