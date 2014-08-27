f90df1d6b96832e3612d9248ac9a7ad36c766548
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-27 16:08:59 -0300

    encuesta

--- includes/gestion/encuesta.php
+++ includes/gestion/encuesta.php
@@ -35,48 +35,40 @@
 <p><strong> a.</strong> Comunicaciones previas en relación a la asignación de fecha, horario de examen y documentación a remitir por el colegio.</p>
 <ul>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1a" value="No Satisfactorio" />
 		<label for="">1</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1a" value="Regular" />
 		<label for="">2</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1a" value="Satisfactorio" />
 		<label for="">3</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1a" value="Muy satisfactorio" />
 		<label for="">4</label>		
-	</li>
-	<li>
-		<input type="radio" name="" value="" />
-		<label for="">5</label>		
 	</li>
 </ul>
 
 <p><strong> b.</strong> Cumplimiento en relación a los horarios pautados.</p>
 <ul>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1b" value="No Satisfactorio" />
 		<label for="">1</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1b" value="Regular" />
 		<label for="">2</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1b" value="Satisfactorio" />
 		<label for="">3</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1b" value="Muy satisfactorio" />
 		<label for="">4</label>		
-	</li>
-	<li>
-		<input type="radio" name="" value="" />
-		<label for="">5</label>		
 	</li>
 </ul>
 <p>En caso de haber rendido bajo la modalidad a distancia, complete también :</p>
@@ -84,48 +76,40 @@
 <p><strong> c.</strong> Comunicaciones previas clarificando y precisando el procedimiento a seguir para el desarrollo de la fecha de examen bajo la modalidad a distancia.</p>
 <ul>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1c" value="No Satisfactorio" />
 		<label for="">1</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1c" value="Regular" />
 		<label for="">2</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1c" value="Satisfactorio" />
 		<label for="">3</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1c" value="Muy satisfactorio" />
 		<label for="">4</label>		
-	</li>
-	<li>
-		<input type="radio" name="" value="" />
-		<label for="">5</label>		
 	</li>
 </ul>
 
 <p><strong> d.</strong> Cumplimiento en los tiempos del envío de los materiales de examen.</p>
 <ul>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1d" value="No Satisfactorio" />
 		<label for="">1</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1d" value="Regular" />
 		<label for="">2</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1d" value="Satisfactorio" />
 		<label for="">3</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_1d" value="Muy satisfactorio" />
 		<label for="">4</label>		
-	</li>
-	<li>
-		<input type="radio" name="" value="" />
-		<label for="">5</label>		
 	</li>
 </ul>
 
@@ -133,24 +117,20 @@
 <p><strong> a.</strong> Cordialidad y asistencia a los alumnos y acompañantes del colegio.</p>
 <ul>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_2a" value="No Satisfactorio" />
 		<label for="">1</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_2a" value="Regular" />
 		<label for="">2</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_2a" value="Satisfactorio" />
 		<label for="">3</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_2a" value="Muy satisfactorio" />
 		<label for="">4</label>		
-	</li>
-	<li>
-		<input type="radio" name="" value="" />
-		<label for="">5</label>		
 	</li>
 </ul>
 
@@ -158,48 +138,40 @@
 <p><strong> a.</strong> Atención a los alumnos.</p>
 <ul>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3a" value="No Satisfactorio" />
 		<label for="">1</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3a" value="Regular" />
 		<label for="">2</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3a" value="Satisfactorio" />
 		<label for="">3</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3a" value="Muy satisfactorio" />
 		<label for="">4</label>		
-	</li>
-	<li>
-		<input type="radio" name="" value="" />
-		<label for="">5</label>		
 	</li>
 </ul>
 
 <p><strong> b.</strong> Devolución de los resultados.</p>
 <ul>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3b" value="No Satisfactorio" />
 		<label for="">1</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3b" value="Regular" />
 		<label for="">2</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3b" value="Satisfactorio" />
 		<label for="">3</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3b" value="Muy satisfactorio" />
 		<label for="">4</label>		
-	</li>
-	<li>
-		<input type="radio" name="" value="" />
-		<label for="">5</label>		
 	</li>
 </ul>
 <p>En caso de haber rendido bajo la modalidad a distancia, conteste también:</p>
@@ -207,24 +179,20 @@
 <p><strong> c.</strong> Asistencia previa a la fecha de examen en el foro.</p>
 <ul>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3c" value="No Satisfactorio" />
 		<label for="">1</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3c" value="Regular" />
 		<label for="">2</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3c" value="Satisfactorio" />
 		<label for="">3</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_3c" value="Muy satisfactorio" />
 		<label for="">4</label>		
-	</li>
-	<li>
-		<input type="radio" name="" value="" />
-		<label for="">5</label>		
 	</li>
 </ul>
 
@@ -232,29 +200,25 @@
 <p>Completar sólo para las fechas de examen rendidas en la UTN-FRBA.</p>
 <ul>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_4a" value="No Satisfactorio" />
 		<label for="">1</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_4a" value="Regular" />
 		<label for="">2</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_4a" value="Satisfactorio" />
 		<label for="">3</label>		
 	</li>
 	<li>
-		<input type="radio" name="" value="" />
+		<input type="radio" name="pregunta_5" value="Muy satisfactorio" />
 		<label for="">4</label>		
-	</li>
-	<li>
-		<input type="radio" name="" value="" />
-		<label for="">5</label>		
 	</li>
 </ul>
 
 <h4><strong>5. A continuación déjenos sus comentarios y/o sugerencias :</strong></h4>
-<textarea name="" rows="5"></textarea>
+<textarea name="pregunta_5a" rows="5"></textarea>
 
 <p><input class="descargar" type="submit" name="" value="Enviar Encuesta"> </p>
 </form>
