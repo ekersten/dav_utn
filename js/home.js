83d21bca2891e04b2072500b3d0bd5661d3fecf6
Eric Kersten <ekersten@gmail.com>
2014-08-27 16:50:46 -0300

    fix home slider para 1024

--- js/home.js
+++ js/home.js
@@ -1,10 +1,17 @@
 (function($){
 	$('.accesos').find('.texto').equalizeHeights();
 	$('.accesos').find('h2').equalizeHeights();
-	$('.cycle-slideshow').find('.interno').each(function(i, el){
-		var $el = $(el),
-			alto = $el.height();
 
-		$(el).css('top', Math.round((488 - alto) / 2));
-	});
+	function onResize() {
+		$('.cycle-slideshow').find('.interno').each(function(i, el){
+			var $el = $(el),
+				alto = $el.height();
+
+			$(el).css('top', Math.round((488 - alto) / 2));
+		});
+	}
+
+	$(window).resize(onResize);
+
+	onResize();
 })(jQuery);
