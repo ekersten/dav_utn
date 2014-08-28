448f4481cfaf1196de27f5fac8f7e28b60d06445
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-28 11:06:24 -0300

    profesores para futuro

---
+++ profesores-futuro.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Profesores para Futuro - DAV UTN</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+		<link rel="stylesheet" href="css/dav.css">
+		<script src="js/vendor/modernizr.js"></script>
+		<!--[if lt IE 9]><script src="js/vendor/rem.min.js"></script><![endif]-->
+	</head>
+	<body>
+		<?php include("includes/header.php"); ?>
+		<section class="tpl-2-col profesores">
+			<div class="header-image"></div>
+			<!-- <div class="subnav" data-activa="1">
+				<?php include("includes/tabs/digital-junior.php"); ?>
+			</div> -->
+			<div class="row contenedor">
+				<div class="column medium-9 pricipal">
+					<h1>Profesores para el Futuro (PPF)</h1>
+					<ul class="breadcrumbs">
+						<li><a href="profesores-futuro.php">Programa de Entes de Gobierno y R.S.E</a></li>
+						<li class="actual">Profesores para el Futuro (PPF)</li>
+						<!-- <li>Información General</li> -->
+					</ul>
+					<p>Es un programa que se propone brindar la capacidad de desarrollar material educativo multimedia de características interactivas para poder ser utilizado a través de soportes off line u on line, soportado en base a software libre.</p>
+					<p>Actualmente PPF está compuesto por tres cursos: CMEM Cs. Básicas, CMEM Genérico y EDM E-learning Docentes. Los contenidos de los diferentes módulos de cada uno de los cursos, fueron confeccionados por docentes universitarios especialistas en los temas. Todos los programas están destinados a Docentes y Profesores de nivel Medio y Universitario.</p>
+					<p>La modalidad de trabajo utilizada en los tres cursos es a través de una plataforma tecnológica de gestión de cursos virtuales, de libre acceso Moodle, pensada como una instancia más de entrenamiento y con un fin socializador. La URL es: <a href="http://www.campushomovidens.com.ar/">http://www.campushomovidens.com.ar/</a></p>
+					<p>Cada curso virtual tiene un espacio de trabajo dentro de la plataforma virtual, con sus respectivos contenidos digitales en diferentes formatos, divididos en módulos y con un grupo de tutores que guían y orientan los aprendizajes.</p>
+					<p>El objetivos de utilizar estas herramientas es, no sólo familiarizar a los docentes en el uso de esta modalidad de enseñanza y aprendizaje, sino de comenzar a generar una comunidad constituida por docentes que forman parte del programa. A través de esta plataforma los profesores pueden comunicarse tanto con sus tutores como con sus colegas para intercambiar experiencias, opiniones, reflexionar y debatir sobre las prácticas educativas. </p>
+					<p>Telecom Argentina es una de las empresas que nos acompaña desde hace 5 años financiando la implementación del programa en todo el país, alcanzando a 1000 docentes  cada año. Se ha desarrollado un sitio con los avances de dicho programa URL: <a href="http://www.campushomovidens.com.ar/">http://www.profesoresparaelfuturo.com/</a>desde donde es posible acceder a material Educativo Multimedia, a la plataforma virtual, como así también, a los proyectos destacados de los años 2007 y 2008.</p>
+					<p>A partir del año 2009 creamos la RED de REFERENTES LOCALES. Esta red está compuesta por profesores que ya se han capacitado y que han tenido una participación destacada en los cursos, desarrollando excelentes proyectos en base a lo aprendido. Estos docentes siguen actualizando a través de esta RED, derramando estos nuevos conocimientos en sus respectivas comunidades.</p>
+			</div>
+			<!-- 	<div class="column medium-3 secundario">
+					<div class="video">
+						<a href="https://www.youtube.com/watch?v=-dxWBb0NVbo" class="fancybox-media"><img src="img/video_dj.jpg" alt=""> </a>
+					</div>
+					<p>El Programa <em>Digital Junior</em> le brinda los siguientes <strong>BENEFICIOS</strong></p>
+					<ul>
+						<li>Contenidos Actualizados</li>
+						<li>Capacitación docente</li>
+						<li>Materiales de estudio</li>
+						<li>Certificación de conocimientos</li>
+						<li>Asesoramiento</li>
+					</ul>
+				</div> -->
+			</div>
+		</section>
+		<?php include("includes/footer.php"); ?>
+		<!-- local scripts -->
+		<script src="js/tabs.js"></script>
+	</body>
+</html>
