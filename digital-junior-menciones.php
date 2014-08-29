033315d7385af42e7fb02619813f8d701ec81f39
Eric Kersten <ekersten@gmail.com>
2014-08-29 12:07:12 -0300

    menciones con angular y fancybox

--- digital-junior-menciones.php
+++ digital-junior-menciones.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" ng-app id="ng-app"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" ng-app id="ng-app"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" ng-app id="ng-app"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" ng-app id="ng-app"> <!--<![endif]-->
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" ng-app="menciones"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" ng-app="menciones"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" ng-app="menciones"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" ng-app="menciones"> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -29,7 +29,7 @@
                         <li><a href="digital-junior-informacion.php">Digital Junior</a></li>
                         <li class="actual">Menciones de Honor</li>
                     </ul>
-                    <div class="menciones" ng-controller="MencionesCtrl">
+                    <div class="menciones" ng-controller="MencionesController">
                         <div class="row">
                             <div class="column small-12">
                                 <h2>{{data.titulo}}</h2>
@@ -38,12 +38,23 @@
                         <div class="row">
                             <div class="column medium-6">
                                 <figure>
-                                    <img src="{{data.cover.foto}}" alt="{{data.cover.texto}}">
+                                    <img ng-src="{{data.cover.src}}" alt="{{data.cover.texto}}">
                                     <figcaption>{{data.cover.texto}}</figcaption>
                                 </figure>
                             </div>
                             <div class="column medium-6">
                                 <p>{{data.texto}}</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="column small-12">
+                                <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-4 fotos">
+                                    <li ng-repeat="foto in data.fotos">
+                                        <a ng-href="{{foto.src}}" class="fancybox" rel="menciones" data-colegio="{{foto.colegio}}" data-mencion="{{foto.mencion}}" data-ubicacion="{{foto.ubicacion}}" title="{{foto.colegio}}">
+                                            <img ng-src="{{foto.src}}">
+                                        </a>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
@@ -66,7 +77,7 @@
         <?php include("includes/footer.php"); ?>
         <!-- local scripts -->
         <script src="js/tabs.js"></script>
-        <script src="js/menciones.js"></script>
         <script src="js/vendor/angular.min.js"></script>
+        <script src="js/menciones.js"></script>
     </body>
 </html>
