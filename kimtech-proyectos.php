9b1fd07124b771612a681866f7780c9da9e42696
Eric Kersten <ekersten@gmail.com>
2014-08-29 15:18:54 -0300

    cambios textos kimtech

--- kimtech-proyectos.php
+++ kimtech-proyectos.php
@@ -7,7 +7,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>KIMTECH - DAV UTN</title>
-		<meta name="description" content="">
+		<meta name="description" content="Kimtech permite integrar conocimientos de distintas asignaturas, aplicándolos a situaciones reales. Estimulando la capacidad creativa y desarrollando un pensamiento ordenado, tendiente a la búsqueda de soluciones.">
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
 		<link rel="stylesheet" href="css/dav.css">
@@ -36,14 +36,7 @@
 					<div class="video">
                         <a href="https://www.youtube.com/watch?v=Y5P8q-_GKgE" class="fancybox-media"><img src="img/video_kimtech.jpg" alt=""> </a>
                     </div>
-					<p>El Programa <em>Digital Junior</em> le brinda los siguientes <strong>BENEFICIOS</strong></p>
-					<ul>
-						<li>Contenidos Actualizados</li>
-						<li>Capacitación docente</li>
-						<li>Materiales de estudio</li>
-						<li>Certificación de conocimientos</li>
-						<li>Asesoramiento</li>
-					</ul>
+					<p><em>Kimtech</em> permite integrar conocimientos de distintas asignaturas, aplicándolos a situaciones reales. Estimulando la capacidad creativa y desarrollando un pensamiento ordenado, tendiente a la búsqueda de soluciones.</p>
 				</div>
 			</div>
 		</section>
