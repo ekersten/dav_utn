60703b898019f22b076e8194174d4529efc29321
Eric Kersten <ekersten@gmail.com>
2014-08-29 15:19:13 -0300

    cambios textos/imagenes

--- quienes_somos.php
+++ quienes_somos.php
@@ -7,7 +7,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>Quienes Somos - DAV UTN</title>
-		<meta name="description" content="">
+		<meta name="description" content="El Departamento de Aprendizaje Visual, fue concebido en el año 2004 como punto de unión entre los recursos de la Facultad Regional Buenos Aires de la Universidad Tecnológica Nacional y la necesidad de soluciones en Nuevas Tecnologías Aplicadas a la Educación de toda la Comunidad Educativa. Generando puentes entre la escuela media y la Universidad, con el objetivo de lograr una mejora en la calidad educativa, privilegiando la aplicación de estrategias no tradicionales, a través de la integración de NTICs en las propuestas educativas, teniendo en cuenta los contextos de aplicación.">
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
 		<link rel="stylesheet" href="css/dav.css">
@@ -26,9 +26,10 @@
 						<li><a href="#">Digital Junior</a></li>
 						<li>Información General</li>
 					</ul> -->
-					<blockquote>&ldquo;Un espacio especialmente creado para la articulación entre la Universidad y las Escuelas.&rdquo;</blockquote>
-					<p>DAV – Departamento de Aprendizaje Visual, a través de su proyecto HOMOVIDENS, se concibió en el año 2002 como punto de unión entre los recursos de la Facultad Regional Buenos Aires de la Universidad Tecnológica Nacional y las necesidades de soluciones en Nuevas Tecnologías Aplicadas a la Educación de toda la Comunidad Educativa. Generando puentes entre la escuela media y la Universidad con el objetivo de contribuir a colocar a la educación a la altura del actual desarrollo Científico-Tecnológico, privilegiando la aplicación de estrategias no tradicionales integrando TIC en las propuestas educativas acordes a los contextos de aplicación.</p>
-					<p>En la actualidad, y luego de haber recorrido 12 años de trabajo, el Departamento ofrece un proyecto integral formado por varios programas pensados para distintos miembros de la comunidad educativa, en los cuales nos acompañan más de 300 escuelas.</p>
+					<p>El <b>Departamento de Aprendizaje Visual,</b> fue concebido en el año 2004 como punto de unión entre los recursos de la Facultad Regional Buenos Aires de la Universidad Tecnológica Nacional y la necesidad de soluciones en Nuevas Tecnologías Aplicadas a la Educación de toda la Comunidad Educativa. Generando puentes entre la escuela media y la Universidad, con el objetivo de lograr una mejora en la calidad educativa, privilegiando la aplicación de estrategias no tradicionales, a través de la integración de NTICs en las propuestas educativas, teniendo en cuenta los contextos de aplicación.</p>
+					<p>En estos 10 años de trabajo el DAV, ha llevado adelante múltiples acciones, llegando a escuelas de niveles medio y primario de todo el país, de gestión pública y privada, y trabajando también para entes de gobierno y patrocinados por importantes empresas multinacionales. Estas acciones han permitido a la UTN.BA llegar a una extensa comunidad educativa (de directivos, profesores, alumnos y familias), convirtiéndola en un referente de Tecnología y Educación.</p>
+					<p>En la actualidad, el Departamento ofrece un proyecto integral formado por varios programas pensados para distintos miembros de la comunidad educativa, en los cuales nos acompañan más de 700 escuelas.</p>
+
 				</div>
 		<!-- 		<div class="column medium-3 secundario">
 					<div class="video">
