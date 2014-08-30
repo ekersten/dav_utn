0cd26f2785c799d3204baec9f8d849a12ff66a20
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-29 22:55:49 -0300

    organizacion de otros documentos en carpeta + css

--- includes/gestion/reservar.php
+++ includes/gestion/reservar.php
@@ -1,7 +1,7 @@
 <h2>Reservar Fecha</h2>
 <h3>Nueva Reserva</h3>
 <p>Desde aquí podrá descargar la planilla de Solicitud de Reserva, para iniciar de esta manera el proceso de reserva de fecha de examen.</p>
-<p>Haga <a href="img/UTN.BA Solicitud de Reserva de Fecha de Examen.xls">click aquí</a> para descargar la planilla de Solicitud de Reserva.<br/>
+<p>Haga <a href="docs/UTN.BA Solicitud de Reserva de Fecha de Examen.xls">click aquí</a> para descargar la planilla de Solicitud de Reserva.<br/>
 Una vez completada con los datos solicitados deben enviarla por mail a la siguiente dirección : <a href="mailto:dav@sceu.frba.utn.edu.ar">dav@sceu.frba.utn.edu.ar</a></p>
 <h3>Importante</h3>
 <p>A medida que avanza el año es posible que la cantidad de exámenes estimada por el colegio en la planilla de Solicitud de reserva se sitúe fuera del intervalo de vacantes reservadas por la universidad. En este caso, deberán comunicarse con la UTN-FRBA para informar de esta situación. Ante la misma, la universidad podrá proponer al colegio reconvenir su fecha de examen de forma tal de poder satisfacer los nuevos requerimientos. En esta situación es importante que el colegio reconfirme su fecha de examen con la mayor antelación posible, de forma tal de no tener que asimilar cambios a último momento.</p>
