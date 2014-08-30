6a06e98e541ef8ae698bf8a18c89f52a682c2fa0
Flavia Ursino <flavia.ursino@gmail.com>
2014-08-30 12:03:15 -0300

    palabras destacads en escuela 2.0

--- escuela-informacion.php
+++ escuela-informacion.php
@@ -31,14 +31,14 @@
 					</ul>
 					<p>El desafío de incluir la tecnología en la educación está más vigente que nunca, sin embargo seguimos enseñando de la misma forma. Ante esta realidad cabe preguntarnos: ¿seguimos invirtiendo en equipamiento tecnológico y contenidos digitales, sosteniendo las rutinas tradicionales del docente? ¿O comenzamos a transformar la Escuela?</p>
 					<p>ESCUELA 2.0 es un programa desarrollado por la UTN.BA, pensado para transformar la Escuela  valiéndose del potencial de las nuevas tecnologías, para brindar un servicio educativo que trascienda las paredes de sus aulas. Una educación para el siglo XXI.</p>
-					<p>Este programa está pensado con el objetivo de colaborar con el Colegio para lograr un cambio efectivo, contemplando su contexto educativo, los recursos técnicos y humanos. Acompañándolos desde la concepción del nuevo proyecto en la capacitación docente y en el monitoreo de su implementación, logrando instaurar un modelo que transforme a corto plazo las posibilidades educativas de la institución integrando nuevas tecnologías Poniendo el acento en diseñar nuevas estrategias pedagógicas.</p>
+					<p>Este programa está pensado con el objetivo de colaborar con el Colegio para lograr un cambio efectivo, contemplando su contexto educativo, los recursos técnicos y humanos. <span class="destacado">Acompañándolos desde la concepción del nuevo proyecto en la capacitación docente y en el monitoreo de su implementación, logrando instaurar un modelo que transforme a corto plazo las posibilidades educativas </span> de la institución integrando nuevas tecnologías Poniendo el acento en diseñar nuevas estrategias pedagógicas.</p>
 					<p>Un eje de trabajo del programa es la construcción del Campus Virtual del colegio. El mismo contempla una capacitación virtual, que permite a los docentes adquirir herramientas para desarrollar proyectos de aplicación áulica propios a través de una plataforma Moodle, aprendiendo a su vez  a diseñar, administrar y tutorear aulas virtuales.</p>
 					<p>Un segundo eje del programa propone el diseño de un proyecto que permite incorporar innovación pedagógica en diversas áreas de la currícula escolar, teniendo en cuenta los recursos humanos y el contexto que posee la institución. A modo de citar algunos ejemplos:
 						<ul>
-							<li>Simuladores Digitales: posibilitan el armado de laboratorios virtuales para la enseñanza de asignaturas exactas.</li>
-							<li>Realidad Aumentada: materiales digitales con 3D, proyectos de geolocalización, involucrando a las áreas sociales.</li>
-							<li>Análisis de Videos: facilitan ver el mundo real a través de la matemática y la física. La aplicación en los deportes genera un valor agregado en esta metodología.</li>
-							<li>Herramientas Web 2.0: permiten el desarrollo de trabajos colaborativos.</li>
+							<li><span class="destacado">Simuladores Digitales:</span> posibilitan el armado de laboratorios virtuales para la enseñanza de asignaturas exactas.</li>
+							<li><span class="destacado">Realidad Aumentada:</span> materiales digitales con 3D, proyectos de geolocalización, involucrando a las áreas sociales.</li>
+							<li><span class="destacado">Análisis de Videos:</span> facilitan ver el mundo real a través de la matemática y la física. La aplicación en los deportes genera un valor agregado en esta metodología.</li>
+							<li><span class="destacado">Herramientas Web 2.0:</span> permiten el desarrollo de trabajos colaborativos.</li>
 						</ul>
 					</p>
 					<p>Todos los ejes del programa tienen como objetivo que el docente experimente y adquiera nuevas capacidades posibles de transferir a los alumnos. En el hacer, el docente aprende, obtiene confianza y autonomía, logrando crear infinitas y originales propuestas de trabajo dentro y fuera del aula.</p>
