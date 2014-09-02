acde1209073ff6a1074477cbfbe07ec382e38bc5
Eric Kersten <ekersten@gmail.com>
2014-09-01 23:29:39 -0300

    diseño proyectos adaptable

--- ciudadania-proyectos.php
+++ ciudadania-proyectos.php
@@ -32,9 +32,9 @@
 					<p>A lo largo del curso hemos interactuado con los alumnos, proponiéndoles tópicos para la reflexión y el intercambio de opiniones entre pares, contenidos de formación e instancias de autoevaluación, desarrollo de trabajos de investigación, tareas individuales y colaborativas. Ejemplo de lo realizado, son los trabajos presentados por los alumnos en las clases. Algunos de los cuales queremos compartir con Uds.</p>
 					<div ng-controller="ProyectosController" class="proyectos">
 						<ul class="tabs">
-							<li class="tab" id="{{'tab-' + $index}}" ng-repeat="anio in data.anios | orderBy:'tab'" ng-click="tabClick($index)">{{anio.tab}}</li>
+							<li class="tab" id="{{'tab-' + $index}}" ng-repeat="anio in data.anios | orderBy:'tab':true" ng-click="tabClick($index)">{{anio.tab}}</li>
 						</ul>
-						<div id="{{'tab-content-' + $index}}" class="tab-content row" ng-repeat="anio in data.anios | orderBy:'tab'">
+						<div id="{{'tab-content-' + $index}}" class="tab-content row" ng-repeat="anio in data.anios | orderBy:'tab':true">
 							<div class="column small-12">
 								<div class="row">
 									<div class="column small-12">
@@ -42,22 +42,28 @@
 										<p>{{anio.texto}}</p>
 									</div>
 								</div>
-								<div class="proyecto row" ng-repeat="proyecto in anio.proyectos">
+								<div class="row">
 									<div class="column small-12">
-										<div class="row">
-											<div class="column small-12">
-												<h4>{{proyecto.nombre}}</h4>
-												<h6>{{proyecto.sub}}</h6>
-											</div>
-										</div>
-										<div class="row">
-											<div class="column small-3"><img ng-src="{{proyecto.imagen}}" alt="{{proyecto.nombre}}"></div>
-											<div class="column small-9">
-												<p><strong><ng-pluralize count="{{proyecto.autores.length}}" when="{'one': 'Autor', 'other': 'Autores'}"></ng-pluralize>:</strong> <span ng-repeat="autor in proyecto.autores">{{autor.nombre}}{{$last ? '' : ', '}}</span></p>
-												<p>{{proyecto.institucion.nombre}} - {{proyecto.institucion.ubicacion}}</p>
-												<p><a ng-href="{{proyecto.url}}">VerProyecto</a></p>
-											</div>
-										</div>
+										<ul class="small-block-grid-1 large-block-grid-2 grilla">
+											<li class="proyecto"ng-repeat="proyecto in anio.proyectos">
+												<div class="inner">
+													<div class="row">
+														<div class="column small-12">
+															<h4>{{proyecto.nombre}}</h4>
+															<h6>{{proyecto.sub}}</h6>
+														</div>
+													</div>
+													<div class="row info">
+														<div class="column small-4"><img ng-src="{{proyecto.imagen}}" alt="{{proyecto.nombre}}"></div>
+														<div class="column small-8">
+															<p><strong><ng-pluralize count="{{proyecto.autores.length}}" when="{'one': 'Autor', 'other': 'Autores'}"></ng-pluralize>:</strong> <span ng-repeat="autor in proyecto.autores">{{autor.nombre}}{{$last ? '' : ', '}}</span></p>
+															<p>{{proyecto.institucion.nombre}} - {{proyecto.institucion.ubicacion}}</p>
+															<p><a ng-href="{{proyecto.url}}">Ver Proyecto</a></p>
+														</div>
+													</div>
+												</div>
+											</li>
+										</ul>
 									</div>
 								</div>
 							</div>
--- js/proyectos.js
+++ js/proyectos.js
@@ -10,6 +10,10 @@
 			url: $scope.data_src
 		}).success(function(data) {
 			$scope.data = data;
+			setTimeout(function(){
+				$scope.tabClick(0);
+			}, 100);
+			
 		});
 
 		$scope.tabClick = function(tab) {
@@ -17,6 +21,13 @@
 			$('.proyectos .tab-content').css('display', 'none');
 			$('#tab-' + tab).addClass('activa');
 			$('#tab-content-' + tab).css('display', 'block');
-		}
+			$('#tab-content-' + tab).find('.inner').css('height', 'auto');
+			$('#tab-content-' + tab).find('.inner').equalizeHeights();
+		};
+	});
+
+	$(window).resize(function(){
+		$('.proyectos .tab-content .inner').css('height', 'auto');
+		$('.proyectos .tab-content .inner').equalizeHeights();
 	});
 })();
