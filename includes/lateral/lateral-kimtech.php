1d31f80e368aae82bea394254143c2e949aaea36
Flavia Ursino <flavia.ursino@gmail.com>
2014-09-08 14:39:25 -0300

    laterala kimtech

---
+++ includes/lateral/lateral-kimtech.php
@@ -0,0 +1,4 @@
+<div class="video">
+    <a href="https://www.youtube.com/watch?v=Y5P8q-_GKgE" class="fancybox-media"><img src="img/video_kimtech.jpg" alt=""> </a>
+</div>
+<p><em>Kimtech</em> permite integrar conocimientos de distintas asignaturas, aplicándolos a situaciones reales. Estimulando la capacidad creativa y desarrollando un pensamiento ordenado, tendiente a la búsqueda de soluciones.</p>
--- kimtech-opinion.php
+++ kimtech-opinion.php
@@ -33,10 +33,7 @@
 					
 				</div>
 				<div class="column medium-3 secundario">
-					<div class="video">
-                        <a href="https://www.youtube.com/watch?v=Y5P8q-_GKgE" class="fancybox-media"><img src="img/video_kimtech.jpg" alt=""> </a>
-                    </div>
-					<p><em>Kimtech</em> permite integrar conocimientos de distintas asignaturas, aplicándolos a situaciones reales. Estimulando la capacidad creativa y desarrollando un pensamiento ordenado, tendiente a la búsqueda de soluciones.</p>
+					<?php include("includes/lateral/lateral-kimtech.php"); ?>
 				</div>
 			</div>
 		</section>
