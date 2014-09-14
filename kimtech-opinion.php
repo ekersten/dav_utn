0474f21d7e15499edda192739bbbb89015d938f6
Flavia Ursino <flavia.ursino@gmail.com>
2014-09-14 11:26:27 -0300

    entrevista Kimtech

--- kimtech-opinion.php
+++ kimtech-opinion.php
@@ -29,9 +29,35 @@
 						<li><a href="kimtech-informacion.php">Kimtech</a></li>
 						<li class="actual">Experiencias</li>
 					</ul>
-					<h2>PROXIMAMENTE</h2>
-					
-				</div>
+					<div class="entrevistas">
+						<div class="entrevista">
+							<div class="titulo">
+								<h3>Entrevista: Escuela Nueva, Trelew</h3>
+							</div>
+							<div class="contenido">
+								<h4><span class="color">Nombre:</span> Franco Di Martino <br/> <span class="color">Puesto:</span> Referente TIC – Profesor del Taller de Robótica KIMTECH</h4>
+								<h5>Por favor, presente la institución, describiendo brevemente su contexto educativo.</h5>
+								<p>La Escuela es un colegio público de gestión privada. […] Se caracteriza por tener un excelente nivel en el área inglés, en el cual nuestros alumnos rinden los exámenes de Cambridge. En informática mediante sus 2 programas (Digital Junior y Kimtech) y en deportes.</p>
+								<p>[…]Su  misión es brindar a los alumnos entre los 3 y 18 años, una educación de excelencia, a través de la ejecución de  programas curriculares provinciales y otros que respondan a la filosofía de la Escuela. Se esfuerza en una actualización permanentemente de la calidad en la enseñanza y aprendizaje, en la administración y manejo de los recursos de la Institución, para que sus alumnos desarrollen al máximo su potencial. Nuestro objetivo es promover el desarrollo individual, el pensamiento independiente y el espíritu deportivo y artístico.</p>
+								<p>[…]En informática se trabaja en una sala actualizada con computadoras personalizadas para cada alumno y en relación directa con la UTN, ya que en diferentes momentos durante el ciclo lectivo, los alumnos rinden  los exámenes correspondientes en el marco del Programa Digital Junior, certificando así sus conocimientos y competencias en informática. Además, integramos las TICS con los espacios curriculares con docentes de informática trabajando en pareja pedagógica con los de los demás espacios curriculares, fomentando así, el uso de estas herramientas en todas las materias. </p>
+								<p>*Sitio de la Escuela Nueva de Trelew: <a href="http://escuelanuevatw.edu.ar/sitio3/3/" target="_blank">http://escuelanuevatw.edu.ar/sitio3/3/</a></p>
+								<h5>¿Por qué decidieron implementar un proyecto de Robótica Educativa en el colegio? ¿Por qué eligieron KimTech?</h5>
+								<p>Creemos que la robótica educativa es un área en el cual nuestros alumnos pueden interrelacionar muchos espacios de su aprendizaje formal, logrando así una formación integral.<br/>Además, somos conscientes de la importancia de la educación en las NTICS para ellos.</p>
+								<p>Elegimos KIMTECH porque vimos en el programa, un programa muy completo y didáctico para lograr nuestros objetivos con respecto al taller.</p>
+								<h5>¿Cómo resultó la capacitación de los docentes?  ¿Qué desafíos se presentaron ante la implementación del programa bajo la modalidad distancia?</h5>
+								<p>La capacitación de los docentes resulto excelente, el docente a cargo por su parte (Baltasar) muy claro, didáctico y predispuesto.<br/> Ante la implementación del programa bajo la modalidad a distancia, el principal desafío quizás será durante este 2014 que nuestros alumnos participaran del coloquio, en el cual es nuestro deseo realizarlo allí en Buenos Aires, en su defecto será vía videoconferencia tal cual fue su propuesta.</p>
+								<h5>¿Cómo les resultaron los materiales de estudio brindados por la UTN.BA (materiales  teóricos y guías de actividades)?</h5>
+								<p>Muy completos y didácticos, fáciles de comprender por los estudiantes y con un excelente hilo conductor.</p>
+								<h5>¿Cómo respondieron los alumnos ante este nuevo proyecto?</h5>
+								<p>Nuestros alumnos respondieron de manera excelente, les encanto el proyecto.<br/>El mayor desafío es lograr vencer la contra que significa presentar el taller en un horario posterior a las 8 horas diarias de clases.</p>
+								<h5>Sabemos que el colegio participó con este proyecto en la Expo Educativa Trelew 2013, ¿Cómo fue la experiencia? ¿Cuál fue la respuesta de la comunidad ante la presentación del trabajo de los alumnos con la robótica? </h5>
+								<p>La respuesta de la comunidad fue muy buena, tuvo una gran repercusión el taller, ya que es totalmente novedoso en la zona. Muchas consultas de personas interesadas.</p>
+								<h5>¿Cómo proyecta la escuela seguir incorporando las NTICs de aquí en adelante?</h5>
+								<p>En proyecto está un taller de StopMotion y ampliar el taller de robótica para realizar trabajos aun más complejos</p>
+							</div>
+						</div>
+					</div>
+			</div>
 				<div class="column medium-3 secundario">
 					<?php include("includes/lateral/lateral-kimtech.php"); ?>
 				</div>
@@ -40,5 +66,6 @@
 		<?php include("includes/footer.php"); ?>
 		<!-- local scripts -->
 		<script src="js/tabs.js"></script>
+		<script src="js/entrevistas.js"></script>
 	</body>
 </html>
