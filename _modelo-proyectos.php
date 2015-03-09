ad996f9877306ca6b8f2f96106a0ee2240de91c9
Eric Kersten <ekersten@gmail.com>
2015-03-09 14:32:39 -0300

    implementacion buscador

--- _modelo-proyectos.php
+++ _modelo-proyectos.php
@@ -24,15 +24,70 @@
 			<div class="subnav" data-activa="proyectos">
 				<?php include("includes/tabs/modelo.php"); ?>
 			</div>
-			<div class="row contenedor">
-				<!-- seccion -->
-				<div ng-view></div>
-				<!-- /seccion -->
+			<div class="row contenedor" ng-controller="MainController">
+				<div class="column medium-12 principal">
+					<div class="row">
+						<div class="column small-4">
+							<h1><a href="#/categorias">Proyectos</a></h1>
+						</div>
+						<div class="column small-4 text-right">
+							<h1><span class="title-icon" ng-class="'icon-' + categoria.slug"></span>{{ categoria.nombre }}</h1>
+						</div>
+					</div>
+					<div class="row">
+						<div class="column small-12">&nbsp;</div>
+					</div>
+					<div class="row">
+						<div class="column small-8 small-offset-2">
+							<form ng-controller="BuscadorController">
+								<div class="row">
+									<div class="column small-12">
+										<input type="text" ng-model="searchdata.q" placeholder="Tema o palabra clave"/>
+									</div>
+									
+								</div>
+								<div class="row">
+									<div class="column small-4">
+										<select ng-model="searchdata.categoria">
+											<option value="all">Materia / Asignatura</option>
+											<option value="{{ categoria.slug }}" ng-repeat="categoria in categorias">{{ categoria.nombre }}</option>
+										</select>
+									</div>
+									<div class="column small-4">
+										<select ng-model="searchdata.herramienta">
+											<option value="all">Herramienta</option>
+											<option value="{{ herramienta }}" ng-repeat="herramienta in herramientas">{{ herramienta }}</option>
+										</select>
+									</div>
+									<div class="column small-4">
+										<select ng-model="searchdata.anio">
+											<option value="all">Año</option>
+											<option value="{{ anio }}" ng-repeat="anio in anios">{{ anio }}</option>
+										</select>
+									</div>
+								</div>
+								<div class="row">
+									<div class="column small-4 small-offset-8 text-right">
+										<input type="reset" class="button tiny" value="Limpiar"/>
+										<button class="button tiny" ng-click="doSearch()">Buscar</button>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="row">
+						<!-- seccion -->
+						<div ng-view></div>
+						<!-- /seccion -->
+					</div>
+				</div>
+
 			</div>
 		</section>
 		<?php include("includes/footer.php"); ?>
 		<!-- local scripts -->
 		<script src="js/tabs.js"></script>
+		<script src="js/vendor/underscore-min.js"></script>
 		<script src="js/modelo-proyectos.js"></script>
 	</body>
 </html>
--- js/modelo-proyectos.js
+++ js/modelo-proyectos.js
@@ -1,4 +1,6 @@
+/* global window _*/
 (function() {
+	'use strict';
 	var app = angular.module('modeloproyectos', ['ngRoute']);
 	
 	app.config(['$routeProvider', function($routeProvider) {
@@ -10,8 +12,8 @@
 			templateUrl: 'includes/modelo-proyectos.html',
 			controller: 'ProyectosController'
 		}).
-		when('/buscar/:texto', {
-			templateUrl: 'includes/modelo-resultados.html',
+		when('/resultados', {
+			templateUrl: 'includes/modelo-proyectos.html',
 			controller: 'ResultadosController'
 		}).
 		otherwise({
@@ -19,77 +21,225 @@
 		});
 	}]);
 
-	app.controller('CategoriasController', function($scope, $rootScope, $http) {
-		$http({
-			method: 'GET',
-			url: $scope.$parent.categories_src
-		}).success(function(data) {
-			$scope.categorias = data;
-		});
+	app.service('Categorias', ['$rootScope', '$http', function($rootScope, $http) {
+		var service = {
+			items: [],
 
-	});
-
-	app.controller('ProyectosController', function($scope, $http, $rootScope, $routeParams) {
-		$scope.categoryFilter = $routeParams.categoria_slug;
-		$http({
-			method: 'GET',
-			url: $scope.$parent.projects_src
-		}).success(function(data) {
-			$scope.proyectos = data;
-		});
-
-		$http({
-			method: 'GET',
-			url: $scope.$parent.categories_src
-		}).success(function(data) {
-			$scope.categorias = data;
-			for (var i=0; i < $scope.categorias.length; i++) {
-				if ($scope.categorias[i].slug === $routeParams.categoria_slug) {
-					$scope.category = $scope.categorias[i];
-					break;
-				}
+			getData: function() {
+				$http({
+					method: 'GET',
+					url: 'data/modelo-categorias.json'
+				}).success(function(data) {
+					service.items = data;
+					$rootScope.$emit('categorias.update');
+				});
 			}
-		});
-	});
-
-	app.controller('BuscadorController', function($scope) {
-		$scope.doSearch = function(e) {
-			e.preventDefault();
-			window.location.hash = '/buscar/' + $scope.texto;
 		};
-	});
 
-	app.controller('ResultadosController', function($scope, $http, $rootScope, $routeParams) {
-		$scope.texto = $routeParams.texto;
-		$http({
-			method: 'GET',
-			url: $scope.$parent.projects_src
-		}).success(function(data) {
-			$scope.proyectos = data;
-		});
+		return service;
+	}]);
 
-		$scope.searchFilter = function(item) {
-			var match_nombre = false,
-				match_herramienta = false,
-				match_autor = false,
-				match_institucion = false,
-				i;
+	app.service('Proyectos', ['$rootScope', '$http', function($rootScope, $http) {
+		var service = {
+			items: [],
 
-			match_nombre = item.nombre.toLowerCase().indexOf($scope.texto.toLowerCase()) >= 0;
-			match_herramienta = item.herramienta.toLowerCase().indexOf($scope.texto.toLowerCase()) >= 0;
+			getData: function() {
+				$http({
+					method: 'GET',
+					url: 'data/modelo-proyectos.json'
+				}).success(function(data) {
+					service.items = data;
+					$rootScope.$emit('proyectos.update');
+				});
+			}
+		};
 
-			for (i = 0; i < item.autores.length; i++) {
-				if (item.autores[i].nombre.toLowerCase().indexOf($scope.texto.toLowerCase()) >= 0) {
-					match_autor = true;
-					break;
-				}
+		return service;
+	}]);
+
+	app.controller('MainController', ['$rootScope', '$scope', '$location', 'Categorias', 'Proyectos', function($rootScope, $scope, $location, Categorias, Proyectos) {
+		// console.log('main init');
+		Categorias.getData();
+		Proyectos.getData();
+
+		$scope.$on('$routeChangeSuccess', function() {
+			console.log('changed to: ', $location.path());
+			if ($location.path() !== '/resultados') {
+				// clear previous serach data
+				$rootScope.$emit('search.clear');
 			}
 
-			// match_institucion = item.institucion.nombre.toLowerCase().indexOf($scope.texto.toLowerCase()) >= 0 || item.institucion.ubicacion.toLowerCase().indexOf($scope.texto.toLowerCase()) >= 0;
+			if ($location.path() === '/resultados' || $location.path() === '/categorias') {
+				$scope.categoria = {};
+			}
+		});
 
-			return match_nombre || match_herramienta || match_autor/* || match_institucion*/;
+		$rootScope.$on('categoria.change', function(e, categoria) {
+			$scope.categoria = categoria;
+		});
+	}]);
 
-		};
+	app.controller('CategoriasController', function($scope, $rootScope, Categorias) {
+		// console.log('categorias init');
+		$scope.categorias = Categorias.items;
+		$rootScope.$on('categorias.update', function(e) {
+			$scope.categorias = Categorias.items;
+		});
 	});
+
+	app.controller('ProyectosController', ['$rootScope', '$scope', '$routeParams', 'Categorias', 'Proyectos', function($rootScope, $scope, $routeParams, Categorias, Proyectos) {
+		// console.log('proyectos init');
+		$scope.filterProyectos = function() {
+			return _.filter(Proyectos.items, function(proyecto) {
+				return proyecto.categoria === $routeParams.categoria_slug;
+			});
+		};
+
+		$scope.categoria = _.find(Categorias.items, function(categoria) {
+			return categoria.slug === $routeParams.categoria_slug;
+		});
+
+		$rootScope.$emit('categoria.change', $scope.categoria);
+
+		$scope.proyectos = $scope.filterProyectos();
+
+		$rootScope.$on('proyectos.update', function(e) {
+			$scope.proyectos = $scope.filterProyectos();
+		});
+
+	}]);
+
+	app.controller('BuscadorController', ['$rootScope', '$scope', '$location', 'Categorias', 'Proyectos', function($rootScope, $scope, $location, Categorias, Proyectos) {
+		console.log('buscador init');
+		$scope.categorias = Categorias.items;
+
+		$rootScope.$on('categorias.update', function(e) {
+			$scope.categorias = Categorias.items;
+		});
+
+		$scope.getHerramientas = function() {
+			var herramientas = [];
+			_.each(Proyectos.items, function(proyecto) {
+				herramientas.push(proyecto.herramienta);
+			});
+			return _.uniq(herramientas);
+		};
+
+		$scope.getAnios = function() {
+			var anios = [];
+			_.each(Proyectos.items, function(proyecto) {
+				anios.push(proyecto.anio);
+			});
+			return _.uniq(anios);
+		};
+
+		$scope.herramientas = $scope.getHerramientas();
+		$scope.anios = $scope.getAnios();
+
+		$rootScope.$on('proyectos.update', function(e) {
+			$scope.herramientas = $scope.getHerramientas();
+			$scope.anios = $scope.getAnios();
+		});
+
+		$rootScope.$on('search.clear', function(e) {
+			$rootScope.searchdata = {};
+			$scope.searchdata = {};
+			$scope.searchdata.q = ''
+			$scope.searchdata.categoria = 'all';
+			$scope.searchdata.herramienta = 'all';
+			$scope.searchdata.anio = 'all';
+		});
+
+		$scope.doSearch = function() {
+			$rootScope.searchdata = _.clone($scope.searchdata);
+			if ($location.path() === '/resultados') {
+				$rootScope.$emit('search.doSearch');
+			} else {
+				$location.path('resultados');
+			}
+		};
+
+		if (typeof $rootScope.searchdata !== 'undefined') {
+			$scope.searchdata = _.clone($rootScope.searchdata);
+		} else {
+			$scope.searchdata = {};
+			$scope.searchdata.q = ''
+			$scope.searchdata.categoria = 'all';
+			$scope.searchdata.herramienta = 'all';
+			$scope.searchdata.anio = 'all';
+		}
+
+	}]);
+
+	app.controller('ResultadosController', ['$rootScope', '$scope', 'Proyectos', function ($rootScope, $scope, Proyectos) {
+		console.log('resultados init');
+
+		$scope.filterProyectos = function() {
+			return _.filter(Proyectos.items, function(proyecto) {
+				var match_nombre = false,
+					match_categoria = false,
+					match_herramienta = false,
+					match_anio = false,
+					searchdata = $rootScope.searchdata;
+
+				if (typeof searchdata === 'undefined') {
+					return false;
+				}
+
+				if(searchdata.q !== '') {
+					if (proyecto.nombre.toLowerCase().indexOf(searchdata.q.toLowerCase()) >= 0) {
+						match_nombre = true;
+					} else {
+						match_nombre = false;
+					}
+				} else {
+					match_nombre = true;
+				}
+
+				if (searchdata.categoria !== 'all') {
+					if (proyecto.categoria === searchdata.categoria) {
+						match_categoria = true;
+					} else {
+						match_categoria = false;
+					}
+				} else {
+					match_categoria = true;
+				}
+
+				if (searchdata.herramienta !== 'all') {
+					if (proyecto.herramienta === searchdata.herramienta) {
+						match_herramienta = true;
+					} else {
+						match_herramienta = false;
+					}
+				} else {
+					match_herramienta = true;
+				}
+
+				if (searchdata.anio !== 'all') {
+					if (proyecto.anio === searchdata.anio) {
+						match_anio = true;
+					} else {
+						match_anio = false;
+					}
+				} else {
+					match_anio = true;
+				}
+
+				return match_nombre && match_categoria && match_herramienta && match_anio;
+			});
+		};
+
+		$scope.proyectos = $scope.filterProyectos();
+
+		$rootScope.$on('proyectos.update', function(e) {
+			$scope.proyectos = $scope.filterProyectos();
+		})
+
+		$rootScope.$on('search.doSearch', function(e) {
+			$scope.proyectos = $scope.filterProyectos();
+		});
+
+	}]);
 
 })();
