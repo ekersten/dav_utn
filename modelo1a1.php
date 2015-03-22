5586e87b16e324a349f25a1f57b45af716764da3
Flavia Ursino <flavia.ursino@gmail.com>
2015-03-22 20:18:53 -0300

    cambios sitios

--- modelo1a1.php
+++ modelo1a1.php
@@ -7,7 +7,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>Modelo 1 a 1 - DAV UTN</title>
-		<meta name="description" content="Las nuevas tecnologías son uno de los medios que más han contribuido a la globalización, a los cambios vertiginosos y permanentes, y es por eso que su integración en la educación se ha convertido en un reto. Para generar una correcta integración de las TIC en los comienzos de la segunda década del siglo XXI es fundamental e inaplazable la formación de los docentes de cualquier área y nivel educativo.">
+		<meta name="description" content="Las nuevas tecnologías son uno de los medios que más han contribuido a la globalización actual, a los cambios vertiginosos y permanentes, y es por eso que su integración en la educación se ha convertido en un reto. Para generar una correcta integración de las TIC en los comienzos de la segunda década del siglo XXI es fundamental e inaplazable la formación de los docentes de cualquier área y nivel educativo.">
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
 		<link rel="stylesheet" href="css/dav.css">
@@ -19,22 +19,25 @@
 		<?php include("includes/header.php"); ?>
 		<section class="tpl-2-col modelo1a1">
 			<div class="header-image"></div>
+			<div class="subnav" data-activa="informacion">
+				<?php include("includes/tabs/modelo.php"); ?>
+			</div>
 			<div class="row contenedor">
 				<div class="column medium-9 principal">
 					<h1>Modelo 1 a 1</h1>
 					<ul class="breadcrumbs">
-						<li><a href="modelo1a1.php">Programas para organismos públicos y R.S.E.</a></li>
+						<li><a href="modelo1a1.php">Programa de Entes de Gobierno y R.S.E</a></li>
 						<li class="actual">Modelo 1 a 1</li>
 						<!-- <li>Información General</li> -->
 					</ul>
-					<p>Las nuevas  tecnologías  son uno de los medios que más han  contribuido a  la globalizaciónl,  a los cambios vertiginosos y permanentes, y es por eso que su integración en la educación se ha convertido en un reto. Para generar una correcta integración de las TIC en los comienzos de la segunda década del siglo XXI es fundamental e inaplazable la formación de los docentes de cualquier área y nivel educativo. </p>
+					<p>Las nuevas  tecnologías  son uno de los medios que más han  contribuido a  la globalización actual,  a los cambios vertiginosos y permanentes, y es por eso que su integración en la educación se ha convertido en un reto. Para generar una correcta integración de las TIC en los comienzos de la segunda década del siglo XXI es fundamental e inaplazable la formación de los docentes de cualquier área y nivel educativo. </p>
 					<p>Nuestro programa de capacitación fue pensado y diseñado para contribuir con el modelo 1 a 1 porque consideramos al docente como un agente de cambio indispensable para que se produzca la integración de las TIC en las aulas. 
 					<span class="destacado">El objetivo de este modelo es que los docentes adquieran competencias para la integración de la tecnología en forma integral, en particular en las Ciencias Básicas, </span>siendo un excelente modelo de aplicación para Matemática, Física, Química y otras áreas técnicas.</p>
-					<p>La meta es que los docentes no sean simples usuarios y consumidores de software y aplicaciones de la red, sino que adquieran conocimientos técnicos, funcionales, metodológicos y lingüísticos. Además, hemos incluido en todos los trayectos formativos la creación de proyectos que se presentan en coloquios presenciales.</p>
-					<p>El conocimiento lingüístico permite manejar el leguaje universal de la web, para no estar cautivos a los designios de las aplicaciones; permite adquirir la lógica que utilizan los lenguajes informáticos, contribuye a la toma de decisiones y la generación de cambios para beneficios pedagógicos de las herramientas utilizadas y fomentan la actitud crítica.</p>	
+					<p>La meta es que los docentes no sean simples usuarios y consumidores de software y aplicaciones de la red, sino que adquieran conocimientos técnicos, funcionales, metodológicos y lingüísticos. Además hemos incluido en todos los trayectos formativos la creación de proyectos que se presentan en los coloquios presenciales.</p>
+					<p>El conocimiento lingüístico permite manejar el leguaje universal de la web, para no estar cautivos a los designios de las aplicaciones; permite adquirir la lógica que utilizan estos lenguajes informáticos, contribuye a la toma de decisiones y la generación de cambios para beneficios pedagógicos de las herramientas utilizadas y fomentan la actitud crítica.</p>
 					<p><span class="destacado">&ldquo;Simplemente se trata de aprender y enseñar a leer y escribir en el lenguaje de la tecnología intelectual predominante. </span>De este modo, la incorporación masiva de computadoras y otros medios informáticos en las aulas llegará a tener sentido social y culturalmente transformador&rdquo; ( Diego Levis, 2007).</p>
 					<p>Las computadoras por sí solas no generan ningún cambio si no están integradas a las prácticas educativas y a nuevas metodologías didácticas. Con la integración de las TIC a las prácticas educativas  se coadyuva a generar una continuidad entre la realidad que viven los alumnos fuera del aula y la que viven dentro de la escuela.</p>
-					<p>Es por eso que generamos una estrategia de formación integral de docentes, centrada en la funcionalidad, el dominio con actitud crítica, habilidades para resolución de problemas y la creación de materiales educativos con creatividad, para que los docentes estructuren ambientes de aprendizaje de forma no tradicional fusionando las TIC con modalidades pedagógicas que estimulan el trabajo cooperativo, el trabajo en grupo y colaborativo.</p>
+					<p>Es por eso que generamos una estrategia de formación integral de los docentes, centrada en la funcionalidad, el dominio con actitud crítica, habilidades para resolución de problemas y la creación  de materiales educativos con creatividad para que los docentes estructuren ambientes de aprendizaje de forma no tradicional fusionando las TIC con modalidades pedagógicas que estimulan el trabajo cooperativo, el trabajo en grupo y colaborativo.</p>
 				</div>
 			</div>
 		</section>
