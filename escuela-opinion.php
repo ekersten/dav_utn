d865254cd1cd10686a484eaf0f7c7be2f907218f
Flavia Ursino <flavia.ursino@gmail.com>
2015-03-23 15:57:28 -0300

    escuela opinion

--- escuela-opinion.php
+++ escuela-opinion.php
@@ -31,97 +31,13 @@
 						<li><a href="escuela-informacion.php">Escuela 2.0</a></li>
 						<li class="actual">Experiencias</li>
 					</ul>
-					<div class="entrevistas">
-						<div class="entrevista">
-							<div class="titulo">
-								<h3>Entrevista: Colegio Bertrand Russell, Banfield</h3>
-							</div>
-							<div class="contenido">
-								<h4><span class="color">Nombre:</span> Daniel Erusalimsky <br/> <span class="color">Puesto:</span> Representante Legal</h4>
-								<h5>Sabemos que el colegio comenzó  a experimentar con el uso del campus virtual de manera muy temprana, ¿Qué los impulsó a hacerlo? </h5>
-								<p>El convencimiento de todo el equipo directivo respecto a la utilidad de la tecnología.<br/>El colegio tiene la página web www.bertrandrussell.com.ar de difusión de actividades desde el año 2000. En 2005 pensamos en incorporar a la misma una base de datos para subir trabajos prácticos para los alumnos de secundaria e información para padres. En 2008 esta base de datos ya era importante y decidimos crear otra web de servicios www.colegiorussell.edu.ar con hosting en el colegio. A mediados de ese mismo año U.T.N. B.A. nos ofrece un curso para docentes sobre Moodle y montar un campus con hosting en el servidor de la UTN. Dos de nuestros docentes tomaron el curso pero instalamos el campus con hosting en el servidor del colegio.</p>
-								<h5>¿Cómo fue esa primera experiencia para el colegio y los docentes?</h5>
-								<p>Las primeras experiencias de utilización del campus en 2009 fueron muy buenas, dado su gran utilidad durante el prolongado receso debido a la gripe A.</p>
-								<h5>¿Qué los motivó a buscar el asesoramiento de la UTN.BA para continuar con el desarrollo del campus?</h5>
-								<p>Con el tiempo decayó el uso del campus, en lugar de generalizarse entre los docentes. Esto nos llevó a pensar que un nuevo curso para un amplio grupo de 25 docentes, dictado por personal de UTN, podía hacer que su uso se difundiera entre todo el equipo. </p>
-								<h5>¿Qué cambios y beneficios comenzaron a notar a partir de esta nueva etapa?</h5>
-								<p>Los cambios fueron de actitud del equipo docente frente a la nueva tecnología, ya que el curso les permitió descubrir las ventajas de su utilización.</p>
-								<h5>¿Cómo recibieron los docentes la nueva propuesta?</h5>
-								<p>Los docentes hicieron el curso con mucho entusiasmo, ya que el mismo cumplimentaba las expectativas que tenían. Hubo satisfacción también por parte de la profesora Viviana Lloret de la UTN respecto al rendimiento del equipo docente participante.</p>
-								<h5>¿Cómo proyecta el colegio seguir incorporando tecnología educativa de aquí en adelante? </h5>
-								<p>En dos aspectos: mejorando el equipamiento de las aulas acercando el campus a las mismas y continuando con el entrenamiento de los docentes en la utilización de las nuevas tecnologías. <br/>E incluyendo a las familias de jardín y primaria en el uso del campus virtual.</p>
-							</div>
-						</div>
-						<div class="entrevista">
-							<div class="titulo">
-								<h3>Entrevista: Colegio San José, Marcos Paz</h3>
-							</div>
-							<div class="contenido">
-								<h4><span class="color">Nombre:</span> Marina TRUSZKOWSKI  <br/><span class="color">Puesto:</span> Vicedirectora Nivel Secundario </h4>
-								<h5>Por favor, presente la institución, describiendo brevemente su contexto educativo.</h5>
-								<p>El colegio San José de Marcos Paz es un colegio que pertenece a la Congregación Hijas de Nuestra Señora de Luján, ubicado en Marcos Paz (Bs. As.), tercer cordón del con urbano bonaerense. Cuenta con el 100% de subvención estatal, con una cuota mensual por alumno extremadamente baja. Es el colegio privado más antiguo de Marcos Paz, establecido en el pueblo desde 1935.</p>
-								<h5>¿Qué experiencia previa tenía la escuela en relación al uso de las NTICs aplicadas al aula?</h5>
-								<p>Desde el año 2004 el colegio participa del programa “Digital Junior” ofrecido por la universidad. También se ha participado de las capacitaciones docentes ofrecidas por el mismo programa. Algunos alumnos además han llevado a cabo con buenos resultados los cursos de “Robótica” y “Ciudadanía Digital”.<br/> El colegio cuenta con una sala de informática en la que no sólo se dictan clases de la materia específicamente, sino que además es utilizada por varios profesores en las distintas áreas que utilizan la tecnología como recurso.</p>
-								<h5>¿Qué los impulsó a querer desarrollar el campus virtual del colegio?</h5>
-								<p>En el año 2012, tres profesoras de la institución participaron del curso sobre “Plataforma Moodle” por el programa Digital Junior, de las cuales dos de ellas pusieron en práctica sus proyectos el año pasado: Prácticas del lenguaje en 2ºaño y Economía en 5ºaño, ambas con muy buenos resultados, sobre todo en la recepción por parte de los alumnos de esta estrategia de aprendizaje. A partir de allí, y con el asesoramiento previo de quienes tienen a cargo el Departamento de Aprendizaje Visual de la Universidad, es que se toma la decisión institucional de comenzar a capacitar en el tema a más docentes del colegio incluyendo además del nivel secundario, el nivel superior, primario e inicial.</p>
-								<h5>¿Qué valoración hacen respecto  del desarrollo del proyecto de construcción del campus virtual de la escuela, llevado adelante junto a la UTN.BA?</h5>
-								<p>A pesar de que recién este año 2014, implementaremos la utilización de nuestro propio campus virtual, estamos convencidos de que los resultados serán más que satisfactorios, sobre todo teniendo la seguridad que encontraremos siempre el asesoramiento comprometido y de excelencia de la UTN.BA.</p>
-								<h5>¿Qué capacidades incorporaron los docentes de la escuela a través de la propuesta de capacitación?</h5>
-								<p>Los docentes de la escuela afianzaron de una manera increíble el uso de esta herramienta en sus clases, abriéndoles además múltiples posibilidades de uso a futuro, por supuesto a medida que van familiarizándose con la estrategia.<br/>Si bien estamos de acuerdo que el uso de la tecnología no es sinónimo de calidad educativa,  nos acerca considerablemente a la realidad cotidiana que desarrollan nuestros alumnos.</p>
-								<h5>¿Qué resultados concretos lograron a partir de esta capacitación?</h5>
-								<p>En principio la creación de nuestro propio campus virtual, que nos permite comenzar a transitar el camino de Escuela 2.0, que nos da la posibilidad de estar acorde a los nuevos paradigmas de la educación en el mundo.<br/>También acercamos a nuestros docentes al uso de nuevas estrategias de enseñanza.</p>
-								<h5>¿Qué impacto generó  el campus virtual en la comunidad de alumnos y padres de la escuela?</h5>
-								<p>Por el momento tenemos sólo la experiencia de dos cursos, 2º año y 5º año, del año pasado,  que se implementó de una manera piloto en “Prácticas del lenguaje” y “Economía” respectivamente. Los resultados fueron excelentes, aunque con un poco de expectativa de parte de alumnos y padres, por tratarse de una estrategia no muy conocida por ellos.  En las primeras reuniones de padres que hemos hecho hasta la fecha se ha recepcionado muy bien la novedad de implementación de aulas virtuales en cada uno de los cursos del nivel secundario.</p>
-								<h5>Cómo proyecta la escuela seguir incorporando las NTICs de aquí en adelante?</h5>
-								<p>Se proyecta capacitar en Moodle a más docentes de la escuela de todos los niveles con los que contamos (inicial- primaria- secundaria- superior).<br/>Implementar los proyectos de Moodle de los docentes que se capacitaron el año pasado.<br/>Seguir mejorando tecnológicamente nuestra sala de informática.<br/>Participar en la medida de lo posible de otras capacitaciones ofrecidas por la universidad, a partir de contar con nuestro propio campus virtual.</p>
-							</div>
-						</div>
-						<div class="entrevista">
-							<div class="titulo">
-								<h3>Entrevista: Colegio Moseñor Alberti, La Plata</h3>
-							</div>
-							<div class="contenido">
-								<h4><span class="color">Nombre:</span> Pbro. Américo Aguirre <br/> <span class="color">Puesto:</span> Representante Legal y Párroco</h4>
-								<h5>Por favor, presente la institución, describiendo brevemente su contexto educativo.</h5>
-								<p>El Colegio Monseñor Alberti es una institución que alberga a más de 1200 alumnos con un arancel muy accesible para las familias, y nuestro compromiso es devolver ese dinero que las familias depositan en nuestra Institución en propuestas pedagógicas y edilicias de excelencia</p>
-								<h5>¿Qué experiencia previa tenía la escuela en relación al uso de las NTICs aplicadas al aula?</h5>
-								<p>En 3º de Escuela Primaria una docente de más de 20 años implementa el proyecto ¨La tecnología en el aula¨ con el propósito de:</p>
-								<ul>
-									<li>Generar que el uso de la computadora en el aula sirva para innovar y mejorar los procesos de enseñanza y aprendizaje.</li>
-									<li>Propiciar  el aprendizaje de TICs, como sustento a los contenidos del currículum promoviendo el desarrollo del pensamiento del alumno.</li>
-								</ul>
-								<h5>¿Qué los impulsó a querer desarrollar el campus virtual del colegio? </h5>
-								<p>Nos impulso los resultados del proyecto de 3º Escuela Primaria.<br/>Ejemplos que los papás nos hacían llegar, </p>
-								<ol>
-									<li>Ejemplo: Un alumno llega a su casa y ante una actividad de fijación sobre la historia del General San Martín toma la computadora, googlea youtube, pakapaka, busca el video y delante de su mamá comienza a contar la historia junto con el personaje guía, prácticamente sabía de memoria el texto del niño guía. El niño es el más pequeño del curso.  La mamá sorprendida lo lleva al Colegio.</li>
-									<li>Ejemplo: Una alumna luego de días de cargar en una planilla Word junto a sus compañeros el libro de lectura que se lleva a su casa, arma una planilla igual en la computadora de su casa.</li>
-									<li>Ejemplo: una alumna a los días de haber armado una narración conjunta con sus compañeros en un power point a la que luego le agregaron imágenes. Trajo en un pen drive una poesía (leída previamente en el aula) en un power point.</li>
-									<li>Ejemplo: el alumno llega a su casa invita a leer a su mamá la narración que habían escrito con sus compañeros en la pantalla y luego de leer le dice -¨esta dos palabras las dije yo¨-. Le cuenta el aporte y la contribución que había realizada a la hermosa narración.</li>
-									<li>Ejemplo de una actividad: <br/>Conversan sobre coplas. Inventan 3 coplas cada uno. Seleccionan cada alumno la mejor.<br/>Arman un PowerPoint con las 31 coplas. Grafican cada uno su copla elegida. La docente saca foto a las producciones gráficas y las sube a la presentación para iluminar cada copla.<br/></li>
-								</ol>
-								<p>De una clase abierta para las autoridades se comprobó: </p>
-								<ul>
-									<li>Querer todos pasar al frente a resolver ejercicios de matemática motivados por el desafío de enfrentarse a la computadora, sin miedo a equivocarse, intentar una y otra vez la resolución.</li>
-									<li>Realizar una germinación natural y por pantalla virtual ver el crecimiento físico.</li>
-									<li>Desarrollar entre todos una narración y realizar la corrección conjunta visualizando el texto en la pantalla.</li>
-									<li>Lo insólito ante el interés y el entusiasmo de los alumnos ¨se disgustan al tocar el timbre de recreo¨ no queriendo desprenderse de esa situación de enseñanza.</li>
-								</ul>
-								<h5>¿Cómo recibieron los docentes esta iniciativa planteada desde la dirección de la escuela?</h5>
-								<p>Muy motivados por el desafío, algunos con miedo porque el uso de la computadora no era tan familiar.</p>
-								<h5>¿Qué valoración hacen respecto  del desarrollo del proyecto de construcción del campus virtual de la escuela, llevado adelante junto a la UTN.BA?</h5>
-								<p>La UTN.BA brinda un acompañamiento muy importante para lograr lo mejor de cada docente; cada docente siente y recibe una atención personalizada. </p>
-								<h5>¿Qué capacidades incorporaron los docentes de la escuela a través de la propuesta de capacitación?</h5>
-								<p>En un primer momento se igualan conocimientos, el programa pone a todos los docentes en un mismo plano de igualdad.<br/>Finalmente, lo más importante a nuestro entender, dota al docente de la capacidad de poder crear sus propios materiales digitales para realizar actividades y proyectos virtuales. Hoy frente a la gran oferta por parte de las Editoriales, son los docentes los que deciden de que manera van a armar sus aulas virtuales porque poseen el conocimiento para hacerlo.</p>
-								<h5>¿Qué resultados concretos lograron a partir de esta capacitación?</h5>
-								<p>La escuela abierta las 24hs.<br/>Que la escuela forme y enseñe en el campo físico como en el virtual.<br/>Nuevos enfoques: áreas de música y plástica toman la modalidad de taller para que el alumno pueda iniciarse o profundizar el tema que desee, ej: aprender un determinado instrumento desde el Nivel Inicial.</p>
-								<h5>¿Qué impacto generó  el campus virtual en la comunidad de alumnos y padres de la escuela?</h5>
-								<p>Nos encontramos en la fase de comunicación y testeo. El proyecto goza de un gran entusiasmo y al mismo tiempo con los miedos que implica el acompañar a nuestros hijos en recursos no naturalizados por nosotros.</p>
-								<h5>¿Cómo proyecta la escuela seguir incorporando las NTICs de aquí en adelante?</h5>
-								<p>Nos encontramos en la fase de implementación y confiamos que los resultados nos llevarán a continuar explorando para lograr el mejor aprovechamiento de las tecnologías.</p>
-
-							</div>
-						</div>
+					<div class=" ">
+						<img src="img/testimoniosE2.0.png" alt="Testimonios E 2.0" width="1000">
+						
 					</div>
+				</div>
+				<div class="column medium-3 secundario">
+					<?php include("includes/lateral/lateral-escuela.php"); ?>
 				</div>
 			</div>
 		</section>
